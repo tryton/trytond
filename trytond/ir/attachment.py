4965239959e0915e3b1c753846f617b1a7fa8285
Cédric Krier <ced@b2ck.com>
2008-02-12 19:06:51 +0100

    Add data_path config option and use an md5 directory structure to store attachments

--- trytond/ir/attachment.py
+++ trytond/ir/attachment.py
@@ -1,19 +1,59 @@
 "Attachment"
+import os
+import md5
+import base64
 from trytond.osv import fields, OSV
+from trytond.config import CONFIG
 
 class Attachment(OSV):
     "Attachment"
     _name = 'ir.attachment'
     _description = __doc__
+
+    #TODO directory structure is on 2 level, check if it is enough
+    def _datas(self, cursor, user, ids, name, arg, context=None):
+        res = {}
+        db_name = cursor.dbname
+        for attachment in self.browse(cursor, user, ids, context=context):
+            value = False
+            if attachment.digest:
+                digest = attachment.digest
+                filename = os.path.join(CONFIG['data_path'], db_name,
+                        digest[0:2], digest[2:4], digest)
+                file_p = file(filename, 'rb')
+                value = base64.encodestring(file_p.read())
+            res[attachment.id] = value
+        return res
+
+    def _datas_inv(self, cursor, user, obj_id, name, value, args, context=None):
+        if not value:
+            return
+        db_name = cursor.dbname
+        directory = os.path.join(CONFIG['data_path'], db_name)
+        if not os.path.isdir(directory):
+            os.makedirs(directory, 0770)
+        data = base64.decodestring(value)
+        digest = md5.new(data).hexdigest()
+        directory = os.path.join(directory, digest[0:2], digest[2:4])
+        if not os.path.isdir(directory):
+            os.makedirs(directory, 0770)
+        filename = os.path.join(directory, digest)
+        file_p = file(filename, 'wb')
+        file_p.write(data)
+        cursor.execute('UPDATE ir_attachment ' \
+                'SET digest = %s ' \
+                'WHERE id = %d', (digest, obj_id))
+
     _columns = {
-        'name': fields.char('Attachment Name',size=64, required=True),
-        'datas': fields.binary('Data'),
-        'datas_fname': fields.char('Data Filename',size=64),
-        'description': fields.text('Description'),
-        # Not required due to the document module !
-        'res_model': fields.char('Resource Model',size=64, readonly=True),
-        'res_id': fields.integer('Resource ID', readonly=True),
-        'link': fields.char('Link', size=256)
+        'name': fields.Char('Attachment Name',size=64, required=True),
+        'datas': fields.Function(_datas, fnct_inv=_datas_inv, method=True,
+            type='binary', string='Datas'),
+        'datas_fname': fields.Char('Data Filename',size=64),
+        'description': fields.Text('Description'),
+        'res_model': fields.Char('Resource Model',size=64, readonly=True),
+        'res_id': fields.Integer('Resource ID', readonly=True),
+        'link': fields.Char('Link', size=256),
+        'digest': fields.Char('Digest', size=32),
     }
 
 Attachment()
