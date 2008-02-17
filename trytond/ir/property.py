bf1d37e921aa211152d9644e202d437b75e05e4d
Cédric Krier <ced@b2ck.com>
2008-02-17 15:31:19 +0100

    Rename res_id, fields_id into res, field

--- trytond/ir/property.py
+++ trytond/ir/property.py
@@ -7,11 +7,11 @@ class Property(OSV):
     _name = 'ir.property'
     _description = __doc__
     _columns = {
-        'name': fields.char('Name', size=128),
-        'value': fields.reference('Value', selection='models_get2', size=128),
-        'res_id': fields.reference('Resource', selection='models_get', size=128),
+        'name': fields.Char('Name', size=128),
+        'value': fields.Reference('Value', selection='models_get2', size=128),
+        'res': fields.Reference('Resource', selection='models_get', size=128),
         #'company_id': fields.many2one('res.company', 'Company'),
-        'fields_id': fields.many2one('ir.model.field', 'Fields',
+        'field': fields.Many2One('ir.model.field', 'Field',
             ondelete='cascade', required=True)
     }
 
@@ -24,11 +24,11 @@ class Property(OSV):
             ]
 
     def models_get2(self, cursor, user, context=None):
-        model_fields_obj = self.pool.get('ir.model.field')
-        ids = model_fields_obj.search(cursor, user, [('view_load', '=', 1)])
+        model_field_obj = self.pool.get('ir.model.field')
+        ids = model_field_obj.search(cursor, user, [('view_load', '=', 1)])
         res = []
         done = {}
-        for model_field in model_fields_obj.browse(cursor, user, ids,
+        for model_field in model_field_obj.browse(cursor, user, ids,
                 context=context):
             if model_field.relation not in done:
                 res.append([model_field.relation, model_field.relation])
@@ -36,11 +36,11 @@ class Property(OSV):
         return res
 
     def models_get(self, cursor, user, context=None):
-        model_fields_obj = self.pool.get('ir.model.field')
-        ids = model_fields_obj.search(cursor, user, [('view_load', '=', 1)])
+        model_field_obj = self.pool.get('ir.model.field')
+        ids = model_field_obj.search(cursor, user, [('view_load', '=', 1)])
         res = []
         done = {}
-        for model_field in model_fields_obj.browse(cursor, user, ids,
+        for model_field in model_field_obj.browse(cursor, user, ids,
                 context=context):
             if model_field.model_id.id not in done:
                 res.append([model_field.model_id.model,
@@ -52,8 +52,8 @@ class Property(OSV):
         if ids:
             cursor.execute('DELETE FROM ir_model_field ' \
                     'WHERE id IN (' \
-                        'SELECT fields_id FROM ir_property ' \
-                        'WHERE (fields_id IS NOT NULL) ' \
+                        'SELECT field FROM ir_property ' \
+                        'WHERE (field IS NOT NULL) ' \
                             'AND (id IN (' + ','.join([str(x) for x in ids]) + \
                             ')))')
         res = super(Property, self).unlink(cursor, user, ids, context)
@@ -65,17 +65,17 @@ class Property(OSV):
         name: property name
         model: object name
         """
-        model_fields_obj = self.pool.get('ir.model.field')
+        model_field_obj = self.pool.get('ir.model.field')
         res = {}
 
-        fields_id = model_fields_obj.search(cursor, user, [
+        field_id = model_field_obj.search(cursor, user, [
             ('name', '=', name),
             ('model', '=', model),
             ], limit=1, context=context)[0]
 
         default_id = self.search(cursor, user, [
-            ('fields_id', '=', fields_id),
-            ('res_id', '=', False),
+            ('field', '=', field_id),
+            ('res', '=', False),
             ], limit=1, context=context)
         default_val = False
         if default_id:
@@ -90,12 +90,12 @@ class Property(OSV):
             res[obj_id] = default_val
 
         property_ids = self.search(cursor, user, [
-            ('fields_id', '=', fields_id),
-            ('res_id', 'in', [name + ',' + str(obj_id) \
+            ('field', '=', field_id),
+            ('res', 'in', [name + ',' + str(obj_id) \
                     for obj_id in  res_ids]),
             ])
         for prop in self.browse(cursor, user, property_ids):
-            res[int(prop.res_id.split(',')[1])] = (prop.value and \
+            res[int(prop.res.split(',')[1])] = (prop.value and \
                     int(prop.value.split(',')[1])) or False
 
         return res
@@ -104,21 +104,21 @@ class Property(OSV):
         """
         Set property value for res_id
         """
-        model_fields_obj = self.pool.get('ir.model.field')
-        fields_id = model_fields_obj.search(cursor, user, [
+        model_field_obj = self.pool.get('ir.model.field')
+        field_id = model_field_obj.search(cursor, user, [
             ('name', '=', name),
             ('model', '=', model),
             ], limit=1, context=context)[0]
 
         property_ids = self.search(cursor, user, [
-            ('fields_id', '=', fields_id),
-            ('res_id', '=', name + ',' + str(res_id)),
+            ('field', '=', field_id),
+            ('res', '=', name + ',' + str(res_id)),
             ], context=context)
         self.unlink(cursor, property_ids, context=context)
 
         default_id = self.search(cursor, user, [
-            ('fields_id', '=', fields_id),
-            ('res_id', '=', False),
+            ('field', '=', field_id),
+            ('res', '=', False),
             ], limit=1, context=context)
         default_id = False
         if default_id:
@@ -131,9 +131,9 @@ class Property(OSV):
             res = self.create(cursor, user, {
                 'name': name,
                 'value': val,
-                'res_id': model + ',' + str(res_id),
+                'res': model + ',' + str(res_id),
 #                'company_id': company_id,
-                'fields_id': fields_id,
+                'field': field_id,
             }, context=context)
         return res
 
