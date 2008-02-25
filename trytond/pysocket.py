5ba1b6a111de1ed5c97b96e6c9fff1d4451f54d1
Cédric Krier <ced@b2ck.com>
2008-02-25 21:19:32 +0100

    IPv6 compliant

--- trytond/pysocket.py
+++ trytond/pysocket.py
@@ -4,6 +4,16 @@ import cStringIO
 
 DNS_CACHE = {}
 
+_ALLOWED_MODULES = {'datetime': ['datetime']}
+
+def checkfunction(module, klass):
+    if module in _ALLOWED_MODULES and klass in _ALLOWED_MODULES[module]:
+        mod = __import__(module, {}, {}, ['__all__'])
+        _class = getattr(mod, klass)
+        return _class
+    raise ValueError('Not supported: %s/%s' % (module, klass))
+
+
 class PySocketException(Exception):
 
     def __init__(self, code, string):
@@ -15,12 +25,7 @@ class PySocketException(Exception):
 class PySocket:
 
     def __init__(self, sock=None):
-        if sock is None:
-            self.sock = socket.socket(
-            socket.AF_INET, socket.SOCK_STREAM)
-        else:
-            self.sock = sock
-        self.sock.settimeout(120)
+        self.sock = sock
         self.host = None
         self.port = None
 
@@ -30,8 +35,18 @@ class PySocket:
             host, port = buf.split(':')
         if host in DNS_CACHE:
             host = DNS_CACHE[host]
+        if not self.sock:
+            familly = socket.AF_INET
+            if socket.has_ipv6:
+                try:
+                    socket.getaddrinfo(host, int(port), socket.AF_INET6)
+                    familly = socket.AF_INET6
+                except:
+                    pass
+            self.sock = socket.socket(familly, socket.SOCK_STREAM)
+            self.sock.settimeout(120)
         self.sock.connect((host, int(port)))
-        DNS_CACHE[host], port = self.sock.getpeername()
+        DNS_CACHE[host], port = self.sock.getpeername()[:2]
         self.host = host
         self.port = port
 
@@ -48,8 +63,11 @@ class PySocket:
     def reconnect(self):
         if self.host and self.port:
             self.disconnect()
+            familly = socket.AF_INET
+            if socket.has_ipv6:
+                familly = socket.AF_INET6
             self.sock = socket.socket(
-            socket.AF_INET, socket.SOCK_STREAM)
+                familly, socket.SOCK_STREAM)
             self.sock.settimeout(120)
             self.sock.connect((self.host, int(self.port)))
 
@@ -86,7 +104,7 @@ class PySocket:
             msg = msg + chunk
         msgio = cStringIO.StringIO(msg)
         unpickler = cPickle.Unpickler(msgio)
-        unpickler.find_global = None
+        unpickler.find_global = checkfunction
         res = unpickler.load()
         if exception:
             raise PySocketException(str(res[0]), str(res[1]))
