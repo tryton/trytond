8815cd7063ffcfd9dd14031b60f5a27c903f54bb
Cédric Krier <ced@b2ck.com>
2008-06-14 12:31:35 +0200

    Remove ack for mx.DateTime.strptime on version 3.0 Remove call to mx.DateTime.strptime

--- trytond/ir/cron.py
+++ trytond/ir/cron.py
@@ -94,8 +94,8 @@ class Cron(OSV, Agent):
                         'AND nextcall <= now() ' \
                         'ORDER BY priority')
             for job in cursor.dictfetchall():
-                nextcall = DateTime.strptime(str(job['nextcall']),
-                        '%Y-%m-%d %H:%M:%S')
+                nextcall = DateTime.mktime(time.strptime(str(job['nextcall']),
+                    '%Y-%m-%d %H:%M:%S'))
                 numbercall = job['numbercall']
                 done = False
                 while nextcall < now and numbercall:
@@ -114,8 +114,7 @@ class Cron(OSV, Agent):
                 cursor.execute("UPDATE ir_cron SET nextcall = %s, " \
                             "numbercall = %s" + addsql + " " \
                             "WHERE id = %s",
-                            (nextcall.strftime('%Y-%m-%d %H:%M:%S'),
-                                numbercall, job['id']))
+                            (nextcall, numbercall, job['id']))
                 cursor.commit()
         finally:
             cursor.close()
