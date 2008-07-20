a67313aba4045dc221847dce826d6120325436ba
Cédric Krier <ced@b2ck.com>
2008-07-20 19:24:49 +0200

    Change password into session more explicit

--- trytond/web_service/object.py
+++ trytond/web_service/object.py
@@ -12,27 +12,27 @@ class Object(Service):
         Service.export_method(self, self.exec_workflow)
         Service.export_method(self, self.obj_list)
 
-    def exec_workflow(self, database, user, passwd, object_name, method,
+    def exec_workflow(self, database, user, session, object_name, method,
             object_id):
-        security.check(database, user, passwd)
+        security.check(database, user, session)
         Cache.clean(database)
         service = LocalService("object_proxy")
         res = service.exec_workflow(database, user, object_name, method,
                 object_id)
         return res
 
-    def execute(self, database, user, passwd, object_name, method, *args):
+    def execute(self, database, user, session, object_name, method, *args):
         if object_name == 'res.request' and method == 'request_get':
-            security.check(database, user, passwd, False)
+            security.check(database, user, session, False)
         else:
-            security.check(database, user, passwd)
+            security.check(database, user, session)
         Cache.clean(database)
         service = LocalService("object_proxy")
         res = service.execute(database, user, object_name, method, *args)
         return res
 
-    def obj_list(self, database, user, passwd):
-        security.check(database, user, passwd)
+    def obj_list(self, database, user, session):
+        security.check(database, user, session)
         Cache.clean(database)
         service = LocalService("object_proxy")
         res = service.obj_list()
