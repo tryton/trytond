3d625c81fea7c1a4cb1cef90139d125714fe1d06
Cédric Krier <ced@b2ck.com>
2008-07-29 10:37:00 +0200

    Remove old name in ir_ui_view table

--- trytond/init.sql
+++ trytond/init.sql
@@ -30,7 +30,6 @@ CREATE TABLE ir_model_field (
 -------------------------------------------------------------------------
 CREATE TABLE ir_ui_view (
 	id serial NOT NULL,
-	name varchar(64) DEFAULT ''::varchar NOT NULL,
 	model varchar(64) DEFAULT ''::varchar NOT NULL,
 	"type" varchar DEFAULT 'form'::varchar NOT NULL,
 	arch text NOT NULL,
