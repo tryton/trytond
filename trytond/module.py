ba2cf992d9098dfd36fb5b09e867296e267d6402
Cédric Krier <ced@b2ck.com>
2008-09-04 18:39:59 +0200

    Add error message about module not found for issue320

--- trytond/module.py
+++ trytond/module.py
@@ -135,6 +135,9 @@ def create_graph(module_list, force=None):
                 raise
             if info.get('installable', True):
                 packages.append((module, info.get('depends', []), info))
+        else:
+            Logger().notify_channel('init', LOG_ERROR,
+                    'module:%s:Module not found!' % (module,))
 
     current, later = Set([x[0] for x in packages]), Set()
     while packages and current > later:
