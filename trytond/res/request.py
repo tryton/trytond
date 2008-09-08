98bc4ffb83f064bcb0e4457646aea9578949c19c
Cédric Krier <ced@b2ck.com>
2008-09-08 15:06:52 +0200

    Remove test on trigger date for request from user for issue334

--- trytond/res/request.py
+++ trytond/res/request.py
@@ -131,9 +131,8 @@ class Request(OSV):
         ids = [x[0] for x in cursor.fetchall()]
         cursor.execute('SELECT id FROM res_request ' \
                 'WHERE act_from = %s AND (act_to <> %s) ' \
-                    'AND (trigger_date <= %s OR trigger_date IS NULL) ' \
                     'AND active = True',
-                    (user, user, time.strftime('%Y-%m-%d')))
+                    (user, user))
         ids2 = [x[0] for x in cursor.fetchall()]
         return (ids, ids2)
 
