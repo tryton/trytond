5e49d2c9f3b06bf59669ebb44396841e17b4a5b2
Cédric Krier <ced@b2ck.com>
2008-09-30 11:55:07 +0200

    Improve sempahore on pooler for issue401

--- trytond/pooler.py
+++ trytond/pooler.py
@@ -12,41 +12,46 @@ _POOL_REPORT = {}
 
 def get_db_and_pool(db_name, update_module=False, wizard=False, report=False,
         lang=None):
+
+    restart = False
     _SEMAPHORE.acquire()
+    try:
+        if db_name in _DB:
+            database = _DB[db_name]
+        else:
+            logging.getLogger('pooler').info(
+                'Connecting to %s' % (db_name))
+            database = db_connect(db_name)
+            cursor = database.cursor()
+            cursor.close()
+            _DB[db_name] = database
 
-    if db_name in _DB:
-        database = _DB[db_name]
-    else:
-        logging.getLogger('pooler').info(
-            'Connecting to %s' % (db_name))
-        database = db_connect(db_name)
-        cursor = database.cursor()
-        cursor.close()
-        _DB[db_name] = database
+        if db_name in _POOL:
+            pool = _POOL[db_name]
+            pool_wizard = _POOL_WIZARD[db_name]
+            pool_report = _POOL_REPORT[db_name]
+        else:
+            logging.getLogger('pooler').info(
+                'Instanciate pooler for %s' % (db_name))
+            from trytond.osv import OSVService
+            pool = OSVService()
+            _POOL[db_name] = pool
 
-    if db_name in _POOL:
-        pool = _POOL[db_name]
-        pool_wizard = _POOL_WIZARD[db_name]
-        pool_report = _POOL_REPORT[db_name]
+            from trytond.wizard import WizardService
+            pool_wizard = WizardService()
+            _POOL_WIZARD[db_name] = pool_wizard
+
+            from trytond.report import ReportService
+            pool_report = ReportService()
+            _POOL_REPORT[db_name] = pool_report
+
+            from trytond.modules import load_modules
+            restart = not load_modules(database, pool, pool_wizard, pool_report,
+                    update_module, lang)
+    finally:
         _SEMAPHORE.release()
-    else:
-        logging.getLogger('pooler').info(
-            'Instanciate pooler for %s' % (db_name))
-        from trytond.osv import OSVService
-        pool = OSVService()
-        _POOL[db_name] = pool
-
-        from trytond.wizard import WizardService
-        pool_wizard = WizardService()
-        _POOL_WIZARD[db_name] = pool_wizard
-
-        from trytond.report import ReportService
-        pool_report = ReportService()
-        _POOL_REPORT[db_name] = pool_report
-
-        _SEMAPHORE.release()
-        from trytond.modules import load_modules
-        load_modules(database, update_module, lang)
+    if restart:
+        restart_pool(db_name)
 
     if wizard:
         return database, pool_wizard
@@ -56,31 +61,37 @@ def get_db_and_pool(db_name, update_module=False, wizard=False, report=False,
 
 def restart_pool(db_name, update_module=False, lang=None):
     _SEMAPHORE.acquire()
-    del _POOL[db_name]
-    del _POOL_WIZARD[db_name]
-    del _POOL_REPORT[db_name]
-    _SEMAPHORE.release()
+    try:
+        del _POOL[db_name]
+        del _POOL_WIZARD[db_name]
+        del _POOL_REPORT[db_name]
+    finally:
+        _SEMAPHORE.release()
     return get_db_and_pool(db_name, update_module=update_module, lang=lang)
 
 def close_db(db_name):
     _SEMAPHORE.acquire()
-    if db_name in _DB:
-        _DB[db_name].close()
-        del _DB[db_name]
-    if db_name in _POOL:
-        del _POOL[db_name]
-    _SEMAPHORE.release()
+    try:
+        if db_name in _DB:
+            _DB[db_name].close()
+            del _DB[db_name]
+        if db_name in _POOL:
+            del _POOL[db_name]
+    finally:
+        _SEMAPHORE.release()
 
 def get_db_only(db_name):
     _SEMAPHORE.acquire()
-    if db_name in _DB:
-        database = _DB[db_name]
-    else:
-        logging.getLogger('pooler').info(
-            'Connecting to %s' % (db_name))
-        database = db_connect(db_name)
-        _DB[db_name] = database
-    _SEMAPHORE.release()
+    try:
+        if db_name in _DB:
+            database = _DB[db_name]
+        else:
+            logging.getLogger('pooler').info(
+                'Connecting to %s' % (db_name))
+            database = db_connect(db_name)
+            _DB[db_name] = database
+    finally:
+        _SEMAPHORE.release()
     return database
 
 def get_db(db_name):
