7248d2eb9cc9ebe4e7aef6302c54c700f97b0cc6
Bertrand Chenal <bch@b2ck.com>
2008-10-15 16:43:44 +0200

    Do not try to insert default values on empty table

--- trytond/sql_db.py
+++ trytond/sql_db.py
@@ -378,13 +378,17 @@ class table_handler:
         column_type = column_type[1]
         self.cursor.execute('ALTER TABLE "%s" ADD COLUMN "%s" %s' %
                        (self.table_name, column_name, column_type))
-        # Populate column with default values:
-        default = None
-        if default_fun is not None:
-            default = default_fun(self.cursor, 0, {})
-        self.cursor.execute('UPDATE "' + self.table_name + '" '\
-                            'SET "' + column_name + '" = ' + symbol_set[0],
-                            (symbol_set[1](default),))
+
+        # check if table is non-empty:
+        self.cursor.execute('SELECT 1 FROM %s limit 1' % self.table_name)
+        if self.cursor.rowcount:
+            # Populate column with default values:
+            default = None
+            if default_fun is not None:
+                default = default_fun(self.cursor, 0, {})
+            self.cursor.execute('UPDATE "' + self.table_name + '" '\
+                                'SET "' + column_name + '" = ' + symbol_set[0],
+                                (symbol_set[1](default),))
 
         self.update_definitions()
 
