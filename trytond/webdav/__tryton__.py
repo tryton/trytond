4efcacc59a1c05aa6b99f92d75115a1231fa8909
Cédric Krier <ced@b2ck.com>
2008-11-05 16:23:28 +0100

    Add description to webdav module for issue503

--- trytond/webdav/__tryton__.py
+++ trytond/webdav/__tryton__.py
@@ -1,7 +1,10 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
 {
     'name': 'WebDAV',
     'category': 'Generic',
+    'description': '''Basic module providing concept and administration of integrated webDAV features.
+''',
     'active': True,
     'depends': ['ir'],
     'xml': [
