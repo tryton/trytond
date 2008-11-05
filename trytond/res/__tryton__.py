2489b9bb99ae87090b49cdbd0b46fe43d1bbc97b
Cédric Krier <ced@b2ck.com>
2008-11-05 16:20:03 +0100

    Add description to res module for issue502

--- trytond/res/__tryton__.py
+++ trytond/res/__tryton__.py
@@ -1,7 +1,11 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
 {
     'name': 'Res',
     'category': 'Generic',
+    'description': '''Basic module handling internal tasks of the application.
+Provides concepts and administration of users and internal communication.
+''',
     'active': True,
     'depends': ['ir'],
     'xml': [
