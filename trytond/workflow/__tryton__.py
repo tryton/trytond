bb52cf1b955f6fce0ce843286136f18cbec6a7dc
Cédric Krier <ced@b2ck.com>
2008-11-05 16:25:11 +0100

    Add description to workflow module for issue504

--- trytond/workflow/__tryton__.py
+++ trytond/workflow/__tryton__.py
@@ -1,7 +1,10 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
 {
     'name': 'Workflow',
     'category': 'Generic',
+    'description': '''Basic module providing concept and administration of workflows.
+''',
     'active': True,
     'depends': ['ir', 'res'],
     'xml': [
