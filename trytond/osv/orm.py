424589ab29cbc2a0a72bb3d0369d6b1991bd4e79
Cédric Krier <ced@b2ck.com>
2008-12-10 16:57:21 +0100

    Fix guidelines

--- trytond/osv/orm.py
+++ trytond/osv/orm.py
@@ -2240,7 +2240,9 @@ class ORM(object):
                         raise AttributeError('Unknown position ' \
                                 'in inherited view %s!' % pos)
                 else:
-                    raise AttributeError('Couldn\'t find tag (%s: %s) in parent view!' % (element2.tag, element2.get('expr')))
+                    raise AttributeError(
+                            'Couldn\'t find tag (%s: %s) in parent view!' % \
+                                    (element2.tag, element2.get('expr')))
             return etree.tostring(tree_src, encoding='utf-8')
 
         result = {'type': view_type, 'model': self._name}
