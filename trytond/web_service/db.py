4a4e3e4b3914f08b59d6ad76f4a90aedc78c2c3c
Cédric Krier <ced@b2ck.com>
2009-01-05 00:25:22 +0100

    Move coding in first line for issue725

--- trytond/web_service/db.py
+++ trytond/web_service/db.py
@@ -1,6 +1,6 @@
+# -*- coding: utf-8 -*-
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
-# -*- coding: utf-8 -*-
 import logging
 import threading
 from trytond.netsvc import Service
