b11549ad6b2d5212d767c5c4733568a2fa9ea24c
Cédric Krier <ced@b2ck.com>
2009-01-06 16:38:38 +0100

    Fix the prefetching of fields in BrowseRecord for issue730 and improve it to prevent to read twice the same field.

--- trytond/osv/orm.py
+++ trytond/osv/orm.py
@@ -126,20 +126,25 @@ class BrowseRecord(object):
 
             # if the field is a classic one or a many2one,
             # we'll fetch all classic and many2one fields
-            if col._classic_write and not col.translate \
-                    and col._type not in ('text', 'binary'):
+            if col._classic_write:
                 # gen the list of "local" (ie not inherited)
                 # fields which are classic or many2one
                 ffields = [x for x in self._table._columns.items() \
-                        if x[1]._classic_write and not x[1].translate]
+                        if x[1]._classic_write \
+                        and x[0] not in self._data[self._id] \
+                        and ((not x[1].translate \
+                                and x[1]._type not in ('text', 'binary')) \
+                            or x[0] == name)]
                 # gen the list of inherited fields
                 inherits = [(x[0], x[1][2]) for x in \
                         self._table._inherit_fields.items()]
                 # complete the field list with the inherited fields
                 # which are classic or many2one
                 ffields += [x for x in inherits if x[1]._classic_write \
-                        and not x[1].translate \
-                        and x[1]._type not in ('text', 'binary')]
+                        and x[0] not in self._data[self._id] \
+                        and ((not x[1].translate \
+                                and x[1]._type not in ('text', 'binary')) \
+                            or x[0] == name)]
             # otherwise we fetch only that field
             else:
                 ffields = [(name, col)]
