bf3446cb009aee23721866a75cc4f8c991ae6807
Cédric Krier <ced@b2ck.com>
2009-01-13 13:16:51 +0100

    Add exponential timeout on failed login attempts. Thanks to Bruce's comment about the Twitter issue.
    --HG--
    extra : transplant_source : %80%AEp%88c%86kNe%20%F0%CF%03%B3%B6%7D%08%0E%F3%F5

--- trytond/security.py
+++ trytond/security.py
@@ -1,4 +1,5 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
 import pooler
 from config import CONFIG
 import sha
@@ -6,30 +7,41 @@ import time
 import random
 
 _USER_CACHE = {}
+_USER_TRY = {}
 
 def login(dbname, loginname, password, cache=True):
+    _USER_TRY.setdefault(dbname, {})
     cursor = pooler.get_db(dbname).cursor()
     password_sha = sha.new(password).hexdigest()
-    cursor.execute('SELECT id FROM res_user '
-        'WHERE login = %s and password = %s and active',
-        (loginname, password_sha))
-    res = cursor.fetchone()
-    cursor.close()
-    if res:
+    cursor.execute('SELECT id, password, active FROM res_user '
+        'WHERE login = %s', (loginname,))
+    if cursor.rowcount:
+        res = cursor.fetchone()
+        cursor.close()
         user_id = res[0]
         if user_id == 0:
             return False
-        if cache:
-            _USER_CACHE.setdefault(dbname, {})
-            timestamp = time.time()
-            session = str(random.random())
-            _USER_CACHE[dbname].setdefault(user_id, [])
-            _USER_CACHE[dbname][user_id].append((timestamp, session))
-            return (user_id, session)
-        else:
-            return user_id
-    else:
+
+        _USER_TRY[dbname].setdefault(user_id, 0)
+        if res[1] == password_sha and res[2]:
+            _USER_TRY[dbname][user_id] = 0
+            if cache:
+                _USER_CACHE.setdefault(dbname, {})
+                timestamp = time.time()
+                session = str(random.random())
+                _USER_CACHE[dbname].setdefault(user_id, [])
+                _USER_CACHE[dbname][user_id].append((timestamp, session))
+                return (user_id, session)
+            else:
+                return user_id
+        time.sleep(2 ** _USER_TRY[dbname][user_id])
+        _USER_TRY[dbname][user_id] += 1
         return False
+    cursor.close()
+    _USER_TRY[dbname].setdefault(0, 0)
+    time.sleep(2 ** _USER_TRY[dbname][0])
+    _USER_TRY[dbname][0] += 1
+    return False
 
 def check_super(passwd):
     if passwd == CONFIG['admin_passwd']:
