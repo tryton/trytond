4457f7ca31608f718c1e77e6dc71c63981762712
Cédric Krier <ced@b2ck.com>
2009-01-29 19:04:58 +0100

    Fix Cacheable to prevent attributes collision
    --HG--
    extra : transplant_source : %1CK%13%05%7F%8E%3AL%EA%25%28%82m%AF%9F%E2%98Qe%14

--- trytond/osv/osv.py
+++ trytond/osv/osv.py
@@ -201,21 +201,19 @@ class Cacheable(object):
     def __init__(self):
         super(Cacheable, self).__init__()
         self._cache = {}
-        self.name = self._table
-        self.timestamp = None
-        self.max_len = 1024
-        self.timeout = 3600
-        self.lock = Lock()
+        self._max_len = 1024
+        self._timeout = 3600
+        self._lock = Lock()
         Cache._cache_instance.append(self)
 
     def add(self, cursor, key, value):
-        self.lock.acquire()
+        self._lock.acquire()
         try:
             self._cache.setdefault(cursor.dbname, {})
 
             lower = None
-            if len(self._cache[cursor.dbname]) > self.max_len:
-                mintime = time.time() - self.timeout
+            if len(self._cache[cursor.dbname]) > self._max_len:
+                mintime = time.time() - self._timeout
                 for key2 in self._cache[cursor.dbname].keys():
                     last_time = self._cache[cursor.dbname][key2][1]
                     if mintime > last_time:
@@ -223,19 +221,19 @@ class Cacheable(object):
                     else:
                         if not lower or lower[1] > last_time:
                             lower = (key2, last_time)
-            if len(self._cache[cursor.dbname]) > self.max_len and lower:
+            if len(self._cache[cursor.dbname]) > self._max_len and lower:
                 del self._cache[cursor.dbname][lower[0]]
 
             self._cache[cursor.dbname][key] = (value, time.time())
         finally:
-            self.lock.release()
+            self._lock.release()
 
     def invalidate(self, cursor, key):
-        self.lock.acquire()
+        self._lock.acquire()
         try:
             del self._cache[cursor.dbname][key]
         finally:
-            self.lock.release()
+            self._lock.release()
 
     def get(self, cursor, key):
         try:
@@ -244,10 +242,10 @@ class Cacheable(object):
             return None
 
     def clear(self, cursor):
-        self.lock.acquire()
+        self._lock.acquire()
         try:
             self._cache.setdefault(cursor.dbname, {})
             self._cache[cursor.dbname].clear()
-            Cache.reset(cursor.dbname, self.name)
+            Cache.reset(cursor.dbname, self._name)
         finally:
-            self.lock.release()
+            self._lock.release()
