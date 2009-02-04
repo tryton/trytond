78ecf250dd0c11bbf852e73b7ef6bc832f142bc3
Cédric Krier <ced@b2ck.com>
2009-02-04 15:34:03 +0100

    Use cursor query to display wrong SQL

--- trytond/sql_db.py
+++ trytond/sql_db.py
@@ -73,8 +73,7 @@ class FakeCursor(object):
                 res = self.cursor.execute(sql)
         except:
             logger = logging.getLogger('sql')
-            logger.error('Wrong SQL: ' + sql % tuple("'%s'" % unicode(x)
-                for x in params or []))
+            logger.error('Wrong SQL: ' + self.cursor.query)
             raise
         if self.sql_log:
             self.count += 1
