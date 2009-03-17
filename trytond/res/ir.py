f4ba22d6b2af82a8ad1dafa1472ed2b0d0420140
Cédric Krier <ced@b2ck.com>
2009-03-17 19:10:37 +0100

    Use default name for ir Model

--- trytond/res/ir.py
+++ trytond/res/ir.py
@@ -1,17 +1,24 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 from trytond.model import ModelView, ModelSQL, fields
+from trytond.backend import TableHandler
 
 
 class UIMenuGroup(ModelSQL):
     "UI Menu - Group"
     _name = 'ir.ui.menu-res.group'
-    _table = 'ir_ui_menu_group_rel'
     _description = __doc__
     menu_id = fields.Many2One('ir.ui.menu', 'Menu', ondelete='RESTRICT',
             select=1, required=True)
     gid = fields.Many2One('res.group', 'Group', ondelete='CASCADE',
             select=1, required=True)
+
+    def init(self, cursor, module_name):
+        # Migration from 1.0 table name change
+        TableHandler.table_rename(cursor, 'ir_ui_menu_group_rel', self._table)
+        TableHandler.sequence_rename(cursor, 'ir_ui_menu_group_rel_id_seq',
+                self._sequence)
+        super(UIMenuGroup, self).init(cursor, module_name)
 
 UIMenuGroup()
 
@@ -19,12 +26,18 @@ UIMenuGroup()
 class ActionGroup(ModelSQL):
     "Action - Group"
     _name = 'ir.action-res.group'
-    _table = 'ir_action_group_rel'
     _description = __doc__
     action_id = fields.Many2One('ir.action', 'Action', ondelete='RESTRICT',
             select=1, required=True)
     gid = fields.Many2One('res.group', 'Group', ondelete='CASCADE',
             select=1, required=True)
+
+    def init(self, cursor, module_name):
+        # Migration from 1.0 table name change
+        TableHandler.table_rename(cursor, 'ir_action_group_rel', self._table)
+        TableHandler.sequence_rename(cursor, 'ir_action_group_rel_id_seq',
+                self._sequence)
+        super(ActionGroup, self).init(cursor, module_name)
 
 ActionGroup()
 
@@ -32,12 +45,18 @@ ActionGroup()
 class ModelFieldGroup(ModelSQL):
     "Model Field Group Rel"
     _name = 'ir.model.field-res.group'
-    _table = 'ir_model_field_group_rel'
     _description = __doc__
     field_id = fields.Many2One('ir.model.field', 'Model Field',
             ondelete='RESTRICT', select=1, required=True)
     group_id = fields.Many2One('res.group', 'Group', ondelete='CASCADE',
             select=1, required=True)
+
+    def init(self, cursor, module_name):
+        # Migration from 1.0 table name change
+        TableHandler.table_rename(cursor, 'ir_model_field_group_rel', self._table)
+        TableHandler.sequence_rename(cursor, 'ir_model_field_group_rel_id_seq',
+                self._sequence)
+        super(ModelFieldGroup, self).init(cursor, module_name)
 
 ModelFieldGroup()
 
@@ -45,12 +64,18 @@ ModelFieldGroup()
 class RuleGroupGroup(ModelSQL):
     "Rule Group - Group"
     _name = 'ir.rule.group-res.group'
-    _table = 'group_rule_group_rel'
     _description = __doc__
     rule_group_id = fields.Many2One('ir.rule.group', 'Rule Group',
             ondelete='RESTRICT', select=1, required=True)
     group_id = fields.Many2One('res.group', 'Group', ondelete='CASCADE',
             select=1, required=True)
+
+    def init(self, cursor, module_name):
+        # Migration from 1.0 table name change
+        TableHandler.table_rename(cursor, 'group_rule_group_rel', self._table)
+        TableHandler.sequence_rename(cursor, 'group_rule_group_rel_id_seq',
+                self._sequence)
+        super(RuleGroupGroup, self).init(cursor, module_name)
 
 RuleGroupGroup()
 
@@ -58,11 +83,17 @@ RuleGroupGroup()
 class RuleGroupUser(ModelSQL):
     "Rule Group - User"
     _name = 'ir.rule.group-res.user'
-    _table = 'user_rule_group_rel'
     _description = __doc__
     rule_group_id = fields.Many2One('ir.rule.group', 'Rule Group',
             ondelete='RESTRICT', select=1, required=True)
     user_id = fields.Many2One('res.user', 'User', ondelete='CASCADE',
             select=1, required=True)
+
+    def init(self, cursor, module_name):
+        # Migration from 1.0 table name change
+        TableHandler.table_rename(cursor, 'user_rule_group_rel', self._table)
+        TableHandler.sequence_rename(cursor, 'user_rule_group_rel_id_seq',
+                self._sequence)
+        super(RuleGroupUser, self).init(cursor, module_name)
 
 RuleGroupUser()
