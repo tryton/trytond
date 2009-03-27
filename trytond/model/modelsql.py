8341fd4fd71489d08983fdf68fc76a1e8272f978
Cédric Krier <ced@b2ck.com>
2009-03-27 11:54:34 +0100

    Enforce uniq name of table alias in join for order statements

--- trytond/model/modelsql.py
+++ trytond/model/modelsql.py
@@ -1560,20 +1560,21 @@ class ModelSQL(ModelStorage):
                 if field_name:
                     order_by, tables, tables_args = obj._order_calc(cursor,
                             user, field_name, otype, context=context)
-                    if table_name == self._table:
-                        table_join = 'LEFT JOIN "' + table_name + '" AS ' \
-                                '"' + table_name + '.' + link_field + '" ON ' \
-                                '"%s.%s".id = "%s".%s' % (table_name, link_field,
-                                        self._table, link_field)
-                        for i in range(len(order_by)):
-                            if table_name in order_by[i]:
-                                order_by[i] = order_by[i].replace(table_name,
-                                        '"' + table_name + '.' + \
-                                                link_field + '"')
-                    else:
-                        table_join = 'LEFT JOIN "' + table_name + '" ON ' \
-                                '"%s".id = "%s".%s' % (table_name, self._table,
-                                        link_field)
+                    table_join = 'LEFT JOIN "' + table_name + '" AS ' \
+                            '"' + table_name + '.' + link_field + '" ON ' \
+                            '"%s.%s".id = "%s".%s' % (table_name, link_field,
+                                    self._table, link_field)
+                    for i in range(len(order_by)):
+                        if table_name in order_by[i]:
+                            order_by[i] = order_by[i].replace(table_name,
+                                    table_name + '.' + link_field)
+                    for i in range(len(tables)):
+                        if table_name in tables[i]:
+                            args = tables_args[tables[i]]
+                            del tables_args[tables[i]]
+                            tables[i] = tables[i].replace(table_name,
+                                    table_name + '.' + link_field)
+                            tables_args[tables[i]] = args
                     if table_join not in tables:
                         tables.insert(0, table_join)
                     return order_by, tables, tables_args
@@ -1595,15 +1596,39 @@ class ModelSQL(ModelStorage):
                     order_by, tables, tables_args = obj2._order_calc(cursor,
                             user, field_name, otype, context=context)
 
-                    table_join = 'LEFT JOIN "' + table_name + '" ON ' \
-                            '"%s".id = "%s".%s' % \
-                            (table_name, self._table, link_field)
+                    table_join = 'LEFT JOIN "' + table_name + '" AS ' \
+                            '"' + table_name + '.' + link_field + '" ON ' \
+                            '"%s.%s".id = "%s".%s' % \
+                            (table_name, link_field, self._table, link_field)
+                    for i in range(len(order_by)):
+                        if table_name in order_by[i]:
+                            order_by[i] = order_by[i].replace(table_name,
+                                    table_name + '.' + link_field)
+                    for i in range(len(tables)):
+                        if table_name in tables[i]:
+                            args = tables_args[tables[i]]
+                            del tables_args[tables[i]]
+                            tables[i] = tables[i].replace(table_name,
+                                    table_name + '.' + link_field)
+                            tables_args[tables[i]] = args
                     if table_join not in tables:
                         tables.insert(0, table_join)
 
-                    table_join2 = 'LEFT JOIN "' + table_name2 + '" ON ' \
-                            '"%s".id = "%s".%s' % \
-                            (table_name2, obj._table, link_field2)
+                    table_join2 = 'LEFT JOIN "' + table_name2 + '" AS ' \
+                            '"' + table_name2 + '.' + link_field2 + '" ON ' \
+                            '"%s.%s".id = "%s".%s' % \
+                            (table_name2, link_field2, obj._table, link_field2)
+                    for i in range(len(order_by)):
+                        if table_name2 in order_by[i]:
+                            order_by[i] = order_by[i].replace(table_name2,
+                                    table_name2 + '.' + link_field2)
+                    for i in range(len(tables)):
+                        if table_name2 in tables[i]:
+                            args = tables_args[tables[i]]
+                            del tables_args[tables[i]]
+                            tables[i] = tables[i].replace(table_name2,
+                                    table_name2 + '.' + link_field2)
+                            tables_args[tables[i]] = args
                     if table_join2 not in tables:
                         tables.insert(1, table_join2)
                     return order_by, tables, tables_args
@@ -1645,7 +1670,7 @@ class ModelSQL(ModelStorage):
                 else:
                     table_join = 'LEFT JOIN "ir_translation" ' \
                             'AS "%s" ON ' \
-                            '("%s".res_id = %s.id ' \
+                            '("%s".res_id = "%s".id ' \
                                 'AND "%s".name = \'%s,%s\' ' \
                                 'AND "%s".lang = %%s ' \
                                 'AND "%s".type = \'model\' ' \
@@ -1658,8 +1683,8 @@ class ModelSQL(ModelStorage):
                     tables.append(table_join)
                     tables_args[table_join] = [context.get('language') or 'en_US']
                 order_by.append('COALESCE(NULLIF(' \
-                        + translation_table + '.value, \'\'), ' \
-                        + table_name + '.' + field_name + ') ' + otype)
+                        + '"' + translation_table + '".value, \'\'), ' \
+                        + '"' + table_name + '".' + field_name + ') ' + otype)
                 return order_by, tables, tables_args
 
             if field_name in self._columns \
