a050e5ad816eeda055c6228507edaba818dc4f53
Cédric Krier <ced@b2ck.com>
2009-06-24 13:23:02 +0200

    Add gzip encoding for XML-RPC

--- trytond/protocols/xmlrpc.py
+++ trytond/protocols/xmlrpc.py
@@ -11,6 +11,8 @@ import traceback
 import socket
 import sys
 import os
+import gzip
+import StringIO
 
 # convert decimal to float before marshalling:
 from decimal import Decimal
@@ -66,6 +68,70 @@ class GenericXMLRPCRequestHandler:
 class SimpleXMLRPCRequestHandler(GenericXMLRPCRequestHandler,
         SimpleXMLRPCServer.SimpleXMLRPCRequestHandler):
     rpc_paths = None
+    encode_threshold = 1400 # common MTU
+
+    # Copy from SimpleXMLRPCServer.py
+    def do_POST(self):
+        """Handles the HTTP POST request.
+
+        Attempts to interpret all HTTP POST requests as XML-RPC calls,
+        which are forwarded to the server's _dispatch method for handling.
+        """
+
+        # Check that the path is legal
+        if not self.is_rpc_path_valid():
+            self.report_404()
+            return
+
+        try:
+            # Get arguments by reading body of request.
+            # We read this in chunks to avoid straining
+            # socket.read(); around the 10 or 15Mb mark, some platforms
+            # begin to have problems (bug #792570).
+            max_chunk_size = 10*1024*1024
+            size_remaining = int(self.headers["content-length"])
+            L = []
+            while size_remaining:
+                chunk_size = min(size_remaining, max_chunk_size)
+                L.append(self.rfile.read(chunk_size))
+                size_remaining -= len(L[-1])
+            data = ''.join(L)
+
+            # In previous versions of SimpleXMLRPCServer, _dispatch
+            # could be overridden in this class, instead of in
+            # SimpleXMLRPCDispatcher. To maintain backwards compatibility,
+            # check to see if a subclass implements _dispatch and dispatch
+            # using that method if present.
+            response = self.server._marshaled_dispatch(
+                    data, getattr(self, '_dispatch', None)
+                )
+        except: # This should only happen if the module is buggy
+            # internal error, report as HTTP server error
+            self.send_response(500)
+            self.end_headers()
+        else:
+            # got a valid XML RPC response
+            self.send_response(200)
+            self.send_header("Content-type", "text/xml")
+
+            # Handle gzip encoding
+            if 'gzip' in self.headers.get('Accept-Encoding', '').split(',') \
+                    and len(response) > self.encode_threshold:
+                buffer = StringIO.StringIO()
+                output = gzip.GzipFile(mode='wb', fileobj=buffer)
+                output.write(response)
+                output.close()
+                buffer.seek(0)
+                response = buffer.getvalue()
+                self.send_header('Content-Encoding', 'gzip')
+
+            self.send_header("Content-length", str(len(response)))
+            self.end_headers()
+            self.wfile.write(response)
+
+            # shut down the connection
+            self.wfile.flush()
+            self.connection.shutdown(1)
 
 
 class SecureXMLRPCRequestHandler(SimpleXMLRPCRequestHandler):
