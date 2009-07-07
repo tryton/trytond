23c5eff18408b252ff5713917e2089f184f2bdcb
Cédric Krier <ced@b2ck.com>
2009-07-07 19:35:44 +0200

    Add sqlite backend

--- trytond/backend/sqlite/__init__.py
+++ trytond/backend/sqlite/__init__.py
@@ -1,8 +1,6 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 
-from trytond.config import CONFIG
-if CONFIG['db_type'] == 'sqlite':
-    from sqlite import *
-else:
-    from postgresql import *
+from database import *
+from fields import *
+from table import *
---
+++ trytond/backend/sqlite/fields.py
@@ -0,0 +1,196 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+try:
+    import hashlib
+except ImportError:
+    hashlib = None
+    import sha
+
+
+class Field(object):
+
+    @staticmethod
+    def sql_format(value):
+        if value is None or value == False:
+            return None
+        elif isinstance(value, str):
+            return unicode(value, 'utf-8')
+        elif isinstance(value, unicode):
+            return value
+        return unicode(value)
+
+    @staticmethod
+    def sql_type(field):
+        return None
+
+
+class Boolean(Field):
+
+    @staticmethod
+    def sql_format(value):
+        return value and '1' or '0'
+
+    @staticmethod
+    def sql_type(field):
+        return ('BOOLEAN', 'BOOLEAN')
+
+
+class Integer(Field):
+
+    @staticmethod
+    def sql_format(value):
+        return int(value or 0)
+
+    @staticmethod
+    def sql_type(field):
+        return ('INTEGER', 'INTEGER')
+
+
+class BigInteger(Integer):
+
+    @staticmethod
+    def sql_type(field):
+        return ('INTEGER', 'INTEGER')
+
+
+class Char(Field):
+
+
+    @staticmethod
+    def sql_type(field):
+        return ('VARCHAR', 'VARCHAR')
+
+
+class Sha(Field):
+
+    @staticmethod
+    def sql_format(value):
+        if hashlib:
+            return value and hashlib.sha1(value).hexdigest() or ''
+        else:
+            return value and sha.new(value).hexdigest() or ''
+
+    @staticmethod
+    def sql_type(field):
+        return ('VARCHAR', 'VARCHAR(40)')
+
+
+class Text(Field):
+
+    @staticmethod
+    def sql_type(field):
+        return ('TEXT', 'TEXT')
+
+
+class Float(Field):
+
+    @staticmethod
+    def sql_format(value):
+        return float(value or 0.0)
+
+    @staticmethod
+    def sql_type(field):
+        return ('FLOAT', 'FLOAT')
+
+
+class Numeric(Float):
+
+    @staticmethod
+    def sql_type(field):
+        return ('NUMERIC', 'NUMERIC')
+
+
+class Date(Field):
+
+    @staticmethod
+    def sql_type(field):
+        return ('DATE', 'DATE')
+
+
+class DateTime(Field):
+
+    @staticmethod
+    def sql_type(field):
+        return ('TIMESTAMP', 'TIMESTAMP')
+
+
+class Time(Field):
+
+    @staticmethod
+    def sql_type(field):
+        return ('TIME', 'TIME')
+
+
+class Binary(Field):
+
+    @staticmethod
+    def sql_format(value):
+        return value or None
+
+    @staticmethod
+    def sql_type(field):
+        return ('BLOB', 'BLOB')
+
+
+class Selection(Char):
+
+    @staticmethod
+    def sql_type(field):
+        return ('VARCHAR', 'VARCHAR')
+
+
+class Reference(Field):
+
+    @staticmethod
+    def sql_type(field):
+        return ('VARCHAR', 'VARCHAR')
+
+
+class Many2One(Field):
+
+    @staticmethod
+    def sql_format(value):
+        return value and int(value) or None
+
+    @staticmethod
+    def sql_type(field):
+        return ('INTEGER', 'INTEGER')
+
+
+class One2Many(Field):
+    pass
+
+
+class Many2Many(Field):
+    pass
+
+
+class Function(Field):
+    pass
+
+
+class Property(Function):
+    pass
+
+
+FIELDS = {
+    'boolean': Boolean,
+    'integer': Integer,
+    'biginteger': BigInteger,
+    'char': Char,
+    'sha': Sha,
+    'text': Text,
+    'float': Float,
+    'numeric': Numeric,
+    'date': Date,
+    'datetime': DateTime,
+    'time': Time,
+    'binary': Binary,
+    'selection': Selection,
+    'reference': Reference,
+    'many2one': Many2One,
+    'one2many': One2Many,
+    'many2many': Many2Many,
+    'function': Function,
+    'property': Property,
+}
