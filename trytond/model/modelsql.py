48ccca3ba9d1ce3cfc61ec8b6fd62ceb289dfba4
Cédric Krier <ced@b2ck.com>
2009-09-08 09:27:43 +0200

    Backed out changeset 2d20900b6eb3

--- trytond/model/modelsql.py
+++ trytond/model/modelsql.py
@@ -1196,7 +1196,6 @@ class ModelSQL(ModelStorage):
         cache.setdefault(self._name, {})
         for data in datas:
             for i in data.keys():
-                i = i.strip('"')
                 if i in ('_timestamp', '_datetime'):
                     continue
                 field = self._columns[i]
