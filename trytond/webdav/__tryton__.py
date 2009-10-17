d9e1546385effa04e6704d58336bbc84935dc2bd
Carlos Perelló Marín <carlos@pemas.es>
2009-10-17 11:21:42 +0200

    Updated all es_ES translations

--- trytond/webdav/__tryton__.py
+++ trytond/webdav/__tryton__.py
@@ -3,9 +3,12 @@
 {
     'name': 'WebDAV',
     'name_de_DE': 'WebDAV',
+    'name_es_ES': 'WebDAV',
     'description': '''Basic module providing concept and administration of integrated webDAV features.
 ''',
     'description_de_DE': '''Basismodul für Konzept und Administration der integrierten WebDAV-Komponente
+''',
+    'description_es_ES': '''Módulo basico que provee el concepto y la administración de servicios WebDAV integrados.
 ''',
     'active': True,
     'depends': ['ir'],
