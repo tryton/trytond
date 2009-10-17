f665191df066b9d3a396bf332cebab87a38ac78f
Cédric Krier <ced@b2ck.com>
2009-10-17 23:21:21 +0200

    Update fr_FR translation and fix typo

--- trytond/webdav/__tryton__.py
+++ trytond/webdav/__tryton__.py
@@ -4,12 +4,15 @@
     'name': 'WebDAV',
     'name_de_DE': 'WebDAV',
     'name_es_ES': 'WebDAV',
-    'description': '''Basic module providing concept and administration of integrated webDAV features.
+    'name_fr_FR': 'WebDAV',
+    'description': '''Basic module providing concept and administration of integrated WebDAV features.
 ''',
     'description_de_DE': '''Basismodul für Konzept und Administration der integrierten WebDAV-Komponente
 ''',
     'description_es_ES': '''Módulo basico que provee el concepto y la administración de servicios WebDAV integrados.
 ''',
+    'description_fr_FR': '''Module de base fournissant les concepts et l'administration des fonctionnalités integrées de WebDAV.
+'''
     'active': True,
     'depends': ['ir'],
     'xml': [
--- trytond/workflow/__tryton__.py
+++ trytond/workflow/__tryton__.py
@@ -4,11 +4,14 @@
     'name': 'Workflow',
     'name_de_DE': 'Workflow',
     'name_es_ES': 'Flujo de trabajo',
+    'name_fr_FR': 'Workflow',
     'description': '''Basic module providing concept and administration of workflows.
 ''',
     'description_de_DE': '''Basismodul für Konzept und Administration von Workflows
 ''',
     'description_es_ES': '''Módulo básico que provee el concepto y administración de flujos de trabajo.
+''',
+    'description_fr_FR': '''Module de base fournissant les concepts et l'administration de workflows.
 ''',
     'active': True,
     'depends': ['ir', 'res'],
