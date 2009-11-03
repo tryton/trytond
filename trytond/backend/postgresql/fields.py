dc0fee10e6f862236fad9f10df74a67b4ad91855
Cédric Krier <ced@b2ck.com>
2009-11-03 15:19:46 +0100

    Don't convert Date and DateTime into string (transplanted from b23f285b08ee4c94719d71cd0053d8d10fff38ec)
    --HG--
    extra : transplant_source : %B2%3F%28%5B%08%EEL%94q%9Dq%CD%00S%D8%D1%0F%FF8%EC

--- trytond/backend/postgresql/fields.py
+++ trytond/backend/postgresql/fields.py
@@ -110,11 +110,19 @@ class Numeric(Float):
 class Date(Field):
 
     @staticmethod
+    def sql_format(value):
+        return value or None
+
+    @staticmethod
     def sql_type(field):
         return ('date', 'date')
 
 
 class DateTime(Field):
+
+    @staticmethod
+    def sql_format(value):
+        return value or None
 
     @staticmethod
     def sql_type(field):
