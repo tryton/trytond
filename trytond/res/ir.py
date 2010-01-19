9d43580c76929bf90de56d2a8a49606a74e8fe99
Bertrand Chenal <bch@b2ck.com>
2010-01-19 12:23:19 +0100

    Add rules on sequences

--- trytond/res/ir.py
+++ trytond/res/ir.py
@@ -109,3 +109,93 @@ class Lang(ModelSQL, ModelView):
         return res
 
 Lang()
+
+
+class SequenceType(ModelSQL, ModelView):
+    _name = 'ir.sequence.type'
+    groups = fields.Many2Many('ir.sequence.type-res.group', 'sequence_type',
+            'group', 'User Groups',
+            help='Groups allowed to edit the sequences of this type')
+
+SequenceType()
+
+
+class SequenceTypeGroup(ModelSQL):
+    'Sequence Type - Group'
+    _name = 'ir.sequence.type-res.group'
+    _description = __doc__
+    sequence_type = fields.Many2One('ir.sequence.type', 'Sequence Type',
+            ondelete='CASCADE', select=1, required=True)
+    group = fields.Many2One('res.group', 'User Groups',
+            ondelete='CASCADE', select=1, required=True)
+
+    def delete(self, cursor, user, ids, context=None):
+        rule_obj = self.pool.get('ir.rule')
+        res = super(SequenceTypeGroup, self).delete(cursor, user, ids,
+                context=context)
+        # Restart the cache on the domain_get method of ir.rule
+        rule_obj.domain_get(cursor.dbname)
+        return res
+
+    def create(self, cursor, user, vals, context=None):
+        rule_obj = self.pool.get('ir.rule')
+        res = super(SequenceTypeGroup, self).create(cursor, user, vals,
+                context=context)
+        # Restart the cache on the domain_get method of ir.rule
+        rule_obj.domain_get(cursor.dbname)
+        return res
+
+    def write(self, cursor, user, ids, vals, context=None):
+        rule_obj = self.pool.get('ir.rule')
+        res = super(SequenceTypeGroup, self).write(cursor, user, ids, vals,
+                context=context)
+        # Restart the cache on the domain_get method
+        rule_obj.domain_get(cursor.dbname)
+        return res
+
+SequenceTypeGroup()
+
+
+class Sequence(ModelSQL, ModelView):
+    _name = 'ir.sequence'
+    groups = fields.Function('get_groups', type='many2many',
+            relation='res.group', string='User Groups',
+            fnct_search='search_groups')
+
+    def get_groups(self, cursor, user, ids, name, arg, context=None):
+        sequence_type_obj = self.pool.get('ir.sequence.type')
+        sequences= self.browse(cursor, user, ids, context=context)
+        code2seq = {}
+        for sequence in sequences:
+            code2seq.setdefault(sequence.code, []).append(sequence.id)
+
+        sequence_type_ids = sequence_type_obj.search(cursor, user, [
+                ('code', 'in', code2seq.keys()),
+                ], context=context)
+        sequence_types = sequence_type_obj.browse(cursor, user,
+                sequence_type_ids, context=context)
+
+        res = {}
+        for sequence_type in sequence_types:
+            seq_ids = code2seq[sequence_type.code]
+            for seq_id in seq_ids:
+                res.setdefault(seq_id, []).append(sequence_type.id)
+
+        return res
+
+    def search_groups(self, cursor, user, name, args, context=None):
+        sequence_type_obj = self.pool.get('ir.sequence.type')
+        ids = sequence_type_obj.search(cursor, user, args, context=context)
+        seq_types = sequence_type_obj.browse(cursor, user, ids, context=context)
+        codes = set(st.code for st in seq_types)
+        return [('code', 'in', list(codes))]
+
+Sequence()
+
+
+class SequenceStrict(Sequence):
+    # This empty class declaration is needed to inherit the groups
+    # field
+    _name = 'ir.sequence.strict'
+
+SequenceStrict()
