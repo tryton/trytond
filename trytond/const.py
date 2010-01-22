5d229f6fadc46e21b610436bce29ec9351afd531
Cédric Krier <ced@b2ck.com>
2010-01-22 11:44:31 +0100

    Move OPERATORS into const to be able to reload modules

---
+++ trytond/const.py
@@ -0,0 +1,19 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+
+OPERATORS = (
+    'child_of',
+    'not child_of',
+    '=',
+    '!=',
+    'like',
+    'not like',
+    'ilike',
+    'not ilike',
+    'in',
+    'not in',
+    '<=',
+    '>=',
+    '<',
+    '>',
+)
