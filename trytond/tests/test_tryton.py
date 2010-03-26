e9c791247ea1c1ab952053d8ba038aa23511ac25
Cédric Krier <ced@b2ck.com>
2010-03-26 18:29:13 +0100

    Add default logging configuration for tests

--- trytond/tests/test_tryton.py
+++ trytond/tests/test_tryton.py
@@ -3,6 +3,8 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 
+import logging
+logging.basicConfig(level=logging.ERROR)
 import sys, os
 DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
     '..', '..', '..', 'trytond')))
