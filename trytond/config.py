456bfc24c9c72656b7f219ae2b3b99618b7cf8d9
Cédric Krier <ced@b2ck.com>
2012-02-22 11:54:02 +0100

    Allow to customize server timezone
    issue2449
    review246001

--- trytond/config.py
+++ trytond/config.py
@@ -11,6 +11,7 @@ except ImportError:
     sys.modules['cdecimal'] = decimal
 import os
 import ConfigParser
+import time
 
 def get_hostname(netloc):
     if '[' in netloc and ']' in netloc:
@@ -76,8 +77,14 @@ class ConfigManager(object):
             'unoconv': 'pipe,name=trytond;urp;StarOffice.ComponentContext',
             'retry': 5,
             'language': 'en_US',
+            'timezone': time.tzname[0] or time.tzname[1],
         }
         self.configfile = None
+
+    def set_timezone(self):
+        os.environ['TZ'] = self.get('timezone')
+        if hasattr(time, 'tzset'):
+            time.tzset()
 
     def update_cmdline(self, cmdline_options):
         self.options.update(cmdline_options)
