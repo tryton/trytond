295f22a2f992ec29b4b15fd4515766b8fcf66694
Cédric Krier <ced@b2ck.com>
2012-03-02 10:43:59 +0100

    Backed out changeset f5a1d3b56969

--- trytond/backend/fields.py
+++ trytond/backend/fields.py
@@ -99,8 +99,10 @@ class Date(Field):
 
         assert(isinstance(value, datetime.date))
         # Allow datetime with min time for XML-RPC
-        if isinstance(value, datetime.datetime):
-            value = value.date()
+        # datetime must be tested separately because datetime is a
+        # subclass of date
+        assert(not isinstance(value, datetime.datetime)
+                or value.time() == datetime.time())
         return value
 
 
