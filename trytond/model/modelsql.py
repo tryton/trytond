5e850f0d798518456bcd30864ed6c176e43c4e21
Cédric Krier <ced@b2ck.com>
2012-05-05 09:28:41 +0200

    Fix nested order_calc tables_args manipulation
    issue2572
    review340003
    (transplanted from 140087fedf85ceeb8be5d12aed23057a1f1bf74b)
    
    --HG--
    extra : transplant_source : %14%00%87%FE%DF%85%CE%EB%8B%E5%D1%2A%ED%23%05z%1F%1B%F7K

--- trytond/model/modelsql.py
+++ trytond/model/modelsql.py
@@ -1890,8 +1890,7 @@ class ModelSQL(ModelStorage):
                                     table_name + '.' + link_field)
                     for i in range(len(tables)):
                         if table_name in tables[i]:
-                            args = tables_args[tables[i]]
-                            del tables_args[tables[i]]
+                            args = tables_args.pop(tables[i], [])
                             tables[i] = tables[i].replace(table_name,
                                     table_name + '.' + link_field)
                             tables_args[tables[i]] = args
@@ -1926,8 +1925,7 @@ class ModelSQL(ModelStorage):
                                     table_name + '.' + link_field)
                     for i in range(len(tables)):
                         if table_name in tables[i]:
-                            args = tables_args[tables[i]]
-                            del tables_args[tables[i]]
+                            args = tables_args.pop(tables[i], [])
                             tables[i] = tables[i].replace(table_name,
                                     table_name + '.' + link_field)
                             tables_args[tables[i]] = args
@@ -1946,8 +1944,7 @@ class ModelSQL(ModelStorage):
                                     table_name2 + '.' + link_field2)
                     for i in range(1, len(tables)):
                         if table_name2 in tables[i]:
-                            args = tables_args[tables[i]]
-                            del tables_args[tables[i]]
+                            args = tables_args.pop(tables[i], [])
                             tables[i] = tables[i].replace(table_name2,
                                     table_name2 + '.' + link_field2)
                             tables_args[tables[i]] = args
