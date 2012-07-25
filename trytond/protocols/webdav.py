b9f6ebe50877def95a259b62fa16293136018427
Cédric Krier <ced@b2ck.com>
2012-07-25 20:51:36 +0200

    Don't fail on unknown database for WebDAV
    issue2717
    review426002
    (transplanted from 66dfaa06e7f80da00229b88e9b9aae7fb4a1e70b)
    
    --HG--
    extra : transplant_source : f%DF%AA%06%E7%F8%0D%A0%02%29%B8%8E%9B%9A%AE%7F%B4%A1%E7%0B

--- trytond/protocols/webdav.py
+++ trytond/protocols/webdav.py
@@ -529,8 +529,11 @@ class WebDAVAuthRequestHandler(AuthServer.BufferedAuthRequestHandler,
 
     def get_userinfo(self, user, password, command=''):
         dbname = urllib.unquote_plus(self.path.split('/', 2)[1])
-        if not dbname:
-            database = Database().connect()
+        database = Database().connect()
+        cursor = database.cursor()
+        databases = database.list(cursor)
+        cursor.close()
+        if not dbname or dbname not in databases:
             return 1
         user = int(login(dbname, user, password, cache=False))
 
