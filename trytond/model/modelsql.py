bfb6d16c7d5d2b8e0af6be753ec9f36b5d2ab375
Cédric Krier <ced@b2ck.com>
2013-03-04 21:30:17 +0100

    Fix check of _sql_error_messages
    issue3021
    review682002
    (transplanted from b0bd4835cfc875baf1bf8124e7e23c4cc45c3483)

--- trytond/model/modelsql.py
+++ trytond/model/modelsql.py
@@ -372,7 +372,7 @@ class ModelSQL(ModelStorage):
                 for name, _, error in self._sql_constraints:
                     if name in exception[0]:
                         self.raise_user_error(error)
-                for name, error in self._sql_error_messages:
+                for name, error in self._sql_error_messages.iteritems():
                     if name in exception[0]:
                         self.raise_user_error(error)
             raise
@@ -886,7 +886,7 @@ class ModelSQL(ModelStorage):
                     for name, _, error in self._sql_constraints:
                         if name in exception[0]:
                             self.raise_user_error(error)
-                    for name, error in self._sql_error_messages:
+                    for name, error in self._sql_error_messages.iteritems():
                         if name in exception[0]:
                             self.raise_user_error(error)
                 raise
