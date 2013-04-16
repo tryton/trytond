e4352ab337989e3f5fcac588a7f7b1c129876ddb
Cédric Krier <ced@b2ck.com>
2013-04-16 15:38:04 +0200

    Fix clean _language_cache
    review782002
    (transplanted from 2833b069cf76cb603305846fa2bf044ddd0cf286)

--- trytond/model/modelstorage.py
+++ trytond/model/modelstorage.py
@@ -150,7 +150,7 @@ class ModelStorage(Model):
 
         # Clean cursor cache
         for cache in Transaction().cursor.cache.values():
-            for cache in (cache, cache.get('_language_cache', {}).values()):
+            for cache in [cache] + cache.get('_language_cache', {}).values():
                 if self._name in cache:
                     if isinstance(ids, (int, long)):
                         ids = [ids]
@@ -220,7 +220,7 @@ class ModelStorage(Model):
 
         # Clean cursor cache
         for cache in Transaction().cursor.cache.values():
-            for cache in (cache, cache.get('_language_cache', {}).values()):
+            for cache in [cache] + cache.get('_language_cache', {}).values():
                 if self._name in cache:
                     if isinstance(ids, (int, long)):
                         ids = [ids]
