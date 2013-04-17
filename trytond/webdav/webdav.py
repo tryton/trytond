7ccd460f91a44feffa23da22b9e191edfb8bd9d2
Cédric Krier <ced@b2ck.com>
2013-04-17 19:42:39 +0200

    Fix get_data to return str instead of buffer (transplanted from 9d27d8712d6f038e08e8c8b20004233a9d0e0e1f)
    --HG--
    extra : transplant_source : %9D%27%D8q-o%03%8E%08%E8%C8%B2%00%04%23%3A%9D%0E%0E%1F

--- trytond/webdav/webdav.py
+++ trytond/webdav/webdav.py
@@ -508,7 +508,7 @@ class Collection(ModelSQL, ModelView):
                     data = DAV_NotFound
                     try:
                         if attachment.data is not None:
-                            data = attachment.data
+                            data = str(attachment.data)
                     except Exception:
                         pass
                     if attachment.id == object_id:
