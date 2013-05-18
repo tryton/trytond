5f7906f2ce86bdc8a8c2ea9143d4ef45a33bff37
Cédric Krier <ced@b2ck.com>
2013-05-18 19:13:12 +0200

    Fix validation of domain for One2One fields
    issue3206
    review852003
    (transplanted from 3765bc03294580c09839b03fa40ea42459376e2b)
    
    --HG--
    extra : transplant_source : 7e%BC%03%29E%80%C0%989%B0%3F%A4%0E%A4%24Y7n%2B

--- trytond/model/modelstorage.py
+++ trytond/model/modelstorage.py
@@ -1024,7 +1024,7 @@ class ModelStorage(Model):
                             domain = PYSONDecoder(env).decode(pyson_domain)
                             relation_ids = []
                             if record[field_name]:
-                                if field._type in ('many2one',):
+                                if field._type in ('many2one', 'one2one'):
                                     relation_ids.append(record[field_name].id)
                                 else:
                                     relation_ids.extend(
@@ -1042,7 +1042,7 @@ class ModelStorage(Model):
                         relation_ids = []
                         for record in records:
                             if record[field_name]:
-                                if field._type in ('many2one',):
+                                if field._type in ('many2one', 'one2one'):
                                     relation_ids.append(record[field_name].id)
                                 else:
                                     relation_ids.extend(
