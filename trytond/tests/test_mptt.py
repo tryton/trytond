021037057536f2919fb3ddfcddc05678b289f123
Cédric Krier <ced@b2ck.com>
2013-09-03 10:26:23 +0200

    Improve MPTT
    - Remove the order on MPTT
    - Make room with queries using left/right instead of ids
    - Call _update_tree multiple times for small list instead of rebuild the all
      tree
    - Improve test for with many roots
    - Factorize the test to update MPTT
    
    issue1892
    review1007002

--- trytond/tests/test_mptt.py
+++ trytond/tests/test_mptt.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
+import sys
 import unittest
 from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT, \
         install_module
@@ -17,12 +18,10 @@ class MPTTTestCase(unittest.TestCase):
         install_module('test')
         self.mptt = POOL.get('test.mptt')
 
-    def CheckTree(self, parent_id=False, left=0, right=0):
+    def CheckTree(self, parent_id=False, left=-1, right=sys.maxint):
         childs = self.mptt.search([
                 ('parent', '=', parent_id),
-                ])
-        child_index = dict((x.id, i) for i, x in enumerate(childs))
-        childs.sort(key=lambda x: child_index[x.id])
+                ], order=[('left', 'ASC')])
         for child in childs:
             assert child.left > left, \
                 '%s: left %d <= parent left %d' % \
@@ -30,21 +29,21 @@ class MPTTTestCase(unittest.TestCase):
             assert child.left < child.right, \
                 '%s: left %d >= right %d' % \
                 (child, child.left, child.right)
-            assert right == 0 or child.right < right, \
+            assert child.right < right, \
                 '%s: right %d >= parent right %d' % \
                 (child, child.right, right)
             self.CheckTree(child.id, left=child.left,
                 right=child.right)
-        next_left = 0
+        next_left = -1
         for child in childs:
             assert child.left > next_left, \
                 '%s: left %d <= next left %d' % \
                 (child, child.left, next_left)
             next_left = child.right
         childs.reverse()
-        previous_right = 0
+        previous_right = sys.maxint
         for child in childs:
-            assert previous_right == 0 or child.right < previous_right, \
+            assert child.right < previous_right, \
                 '%s: right %d >= previous right %d' \
                 % (child, child.right, previous_right)
             previous_right = child.left
@@ -65,61 +64,27 @@ class MPTTTestCase(unittest.TestCase):
         for record in records:
             self.ReParent(record)
 
-    def ReOrder(self, parent=None):
-        records = self.mptt.search([
-                ('parent', '=', parent),
-                ])
-        if not records:
-            return
-        i = len(records)
-        for record in records:
-            record.sequence = i
-            record.save()
-            i -= 1
-        i = 0
-        for record in records:
-            record.sequence = i
-            record.save()
-            i += 1
-        for record in records:
-            self.ReOrder(record)
-
-        records = self.mptt.search([])
-        self.mptt.write(records, {
-                'sequence': 0,
-                })
-
     def test0010create(self):
         '''
         Create tree.
         '''
         with Transaction().start(DB_NAME, USER,
                 context=CONTEXT) as transaction:
-            new_records = [None]
+            new_records = [None, None, None]
             for j in range(3):
                 parent_records = new_records
                 new_records = []
                 k = 0
                 for parent_record in parent_records:
-                    for i in range(3):
-                        new_records += self.mptt.create([{
-                                    'name': 'Test %d %d %d' % (j, k, i),
-                                    'parent': (parent_record.id
-                                            if parent_record else None),
-                                    }])
+                    new_records += self.mptt.create([{
+                                'name': 'Test %d %d %d' % (j, k, i),
+                                'parent': (parent_record.id
+                                    if parent_record else None),
+                                } for i in range(3)])
                     k += 1
             self.CheckTree()
 
             transaction.cursor.commit()
-
-    def test0020reorder(self):
-        '''
-        Re-order.
-        '''
-        with Transaction().start(DB_NAME, USER,
-                context=CONTEXT) as transaction:
-            self.ReOrder()
-            transaction.cursor.rollback()
 
     def test0030reparent(self):
         '''
@@ -141,8 +106,6 @@ class MPTTTestCase(unittest.TestCase):
             self.CheckTree()
             self.ReParent()
             self.CheckTree()
-            self.ReOrder()
-            self.CheckTree()
 
             transaction.cursor.rollback()
 
@@ -163,8 +126,6 @@ class MPTTTestCase(unittest.TestCase):
                     'active': False
                     })
             self.ReParent()
-            self.CheckTree()
-            self.ReOrder()
             self.CheckTree()
 
             transaction.cursor.rollback()
