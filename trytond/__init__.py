af5575d98cdde8026e8a9259493fa29b5bbbab21
Robin Baumgartner <robin@baumgartners.ch>
2014-11-28 10:59:31 +0100

    Improve readability of logged email messages
    issue3542
    review8751002
    
    --HG--
    extra : rebase_source : 230be1a0c8f9d725f3345e72847e1fe99c047496

--- trytond/__init__.py
+++ trytond/__init__.py
@@ -2,6 +2,7 @@
 #this repository contains the full copyright notices and license terms.
 import os
 import time
+from email import Charset
 
 from . import server
 
@@ -10,3 +11,6 @@ __all__ = ['server']
 os.environ['TZ'] = 'UTC'
 if hasattr(time, 'tzset'):
     time.tzset()
+
+# set email encoding for utf-8 to 'quoted-printable'
+Charset.add_charset('utf-8', Charset.QP, Charset.QP)
