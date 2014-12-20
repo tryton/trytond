6618afe98951bfb322eb2f4dd562b516a1d6ce2e
Cédric Krier <ced@b2ck.com>
2014-12-20 19:24:42 +0100

    Use Field.sql_column for SELECT queries in ModelSQL
    By using sql_column, it increases the possibilities of customisation of fields.
    
    issue4369
    review14711002

--- trytond/model/fields/numeric.py
+++ trytond/model/fields/numeric.py
@@ -1,11 +1,18 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from decimal import Decimal
-from sql import Query, Expression, Cast, Literal, Select, CombiningQuery
+from sql import Query, Expression, Cast, Literal, Select, CombiningQuery, As
 
 from ... import backend
 from .field import SQLType
 from .float import Float
+
+
+class SQLite_Cast(Cast):
+
+    def as_(self, output_name):
+        # Use PARSE_COLNAMES instead of CAST for final column
+        return As(self.expression, '%s [NUMERIC]' % output_name)
 
 
 class Numeric(Float):
@@ -36,7 +43,7 @@ class Numeric(Float):
         db_type = backend.name()
         if db_type == 'sqlite':
             # Must be casted as Decimal is stored as bytes
-            column = Cast(column, self.sql_type().base)
+            column = SQLite_Cast(column, self.sql_type().base)
         return column
 
     def _domain_value(self, operator, value):
