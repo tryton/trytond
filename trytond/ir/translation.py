86ab5a88dfe5a3cef44d2e58cccee761dbaa8795
Ritesh Shrivastav <ritesh.shrv@gmail.com>
2016-11-24 09:28:35 +0100

    Fix wrong key for translation_overridden error message
    issue6052
    review26911002
    (grafted from ca5647c94103b3aaa03f283ab48f618366f45b2e)
    
    --HG--
    branch : 4.0

--- trytond/ir/translation.py
+++ trytond/ir/translation.py
@@ -951,7 +951,7 @@ class Translation(ModelSQL, ModelView):
                     and translation.overriding_module != module):
                 cls.raise_user_error('translation_overridden', {
                         'name': translation.name,
-                        'name': translation.overriding_module,
+                        'overriding_module': translation.overriding_module,
                         })
             flags = [] if not translation.fuzzy else ['fuzzy']
             trans_ctxt = '%(type)s:%(name)s:' % {
