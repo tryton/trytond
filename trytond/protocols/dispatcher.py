17d79b5df96699063e2a0ffab0baf8204fa21eea
Cédric Krier <ced@b2ck.com>
2017-06-18 15:24:05 +0200

    Decode username only if it is bytes
    The decoding is only needed on Python2 because bytes_to_wsgi returns always a
    str but str on Python2 is not unicode.
    
    issue6578
    review41381002
    (grafted from 29dd198098b1063ad436903f473ebb3490b58e53)
    
    --HG--
    branch : 4.0

--- trytond/protocols/dispatcher.py
+++ trytond/protocols/dispatcher.py
@@ -182,7 +182,9 @@ def _dispatch(request, pool, *args, **kwargs):
             % (method, obj))
 
     log_message = '%s.%s(*%s, **%s) from %s@%s/%s'
-    username = request.authorization.username.decode('utf-8')
+    username = request.authorization.username
+    if isinstance(username, bytes):
+        username = username.decode('utf-8')
     log_args = (
         obj, method, args, kwargs, username, request.remote_addr, request.path)
     logger.info(log_message, *log_args)
