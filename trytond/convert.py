67b3b7db86dbb186adc49e08638a7217b205b82a
Nicolas Évrard <nicoe@b2ck.com>
2017-07-27 09:40:14 +0200

    Allow to specify datetime related values in XML files
    issue6660
    review35181002

--- trytond/convert.py
+++ trytond/convert.py
@@ -1,6 +1,7 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 import time
+import datetime
 from xml import sax
 import logging
 import re
@@ -230,6 +231,7 @@ class RecordTagHandler:
                 context['version'] = __version__.rsplit('.', 1)[0]
                 context['ref'] = self.mh.get_id
                 context['Decimal'] = Decimal
+                context['datetime'] = datetime
                 if pyson_attr:
                     context.update(CONTEXT)
                 value = eval(eval_attr, context)
