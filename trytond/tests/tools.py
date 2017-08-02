792a88e34e9d4e1c69175474b41d625fe2224c53
Cédric Krier <ced@b2ck.com>
2017-08-02 19:37:32 +0200

    Add set_user tools for test scenario
    When changing the user of proteus, the context should be reloaded.
    
    issue6652
    review42551002

--- trytond/tests/tools.py
+++ trytond/tests/tools.py
@@ -1,8 +1,10 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-from proteus import config, Model, Wizard
+from proteus import config as pconfig, Model, Wizard
 
 from .test_tryton import restore_db_cache, backup_db_cache, drop_create
+
+__all__ = ['activate_modules', 'set_user']
 
 
 def activate_modules(modules):
@@ -26,6 +28,14 @@ def activate_modules(modules):
 
 
 def _get_config():
-    cfg = config.set_trytond()
+    cfg = pconfig.set_trytond()
     cfg.pool.test = True
     return cfg
+
+
+def set_user(user, config=None):
+    if not config:
+        config = pconfig.get_config()
+    User = Model.get('res.user', config=config)
+    config.user = int(user)
+    config._context = User.get_preferences(True, {})
