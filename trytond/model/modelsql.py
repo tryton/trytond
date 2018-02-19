5587877edfd75a95e70e1163bae8594da18d294f
Cédric Krier <ced@b2ck.com>
2018-02-19 19:09:06 +0100

    Use unicode instead of str for DatabaseIntegrityError
    The exception message could contain non-ASCII errors for which standard
    __str__ may fail, but as the database should always be in UTF-8 we may except
    that unicode will not fail.
    
    issue6128
    review43971002
    (grafted from 01a600ff73c57278d269f6d74f44455643a0e7b6)
    
    --HG--
    branch : 3.8

--- trytond/model/modelsql.py
+++ trytond/model/modelsql.py
@@ -289,10 +289,10 @@ class ModelSQL(ModelStorage):
                     cls.raise_user_error('foreign_model_missing',
                         error_args=error_args)
         for name, _, error in cls._sql_constraints:
-            if name in str(exception[0]):
+            if name in unicode(exception[0]):
                 cls.raise_user_error(error)
         for name, error in cls._sql_error_messages.iteritems():
-            if name in str(exception[0]):
+            if name in unicode(exception[0]):
                 cls.raise_user_error(error)
 
     @classmethod
