44a8334b1935968499d5eb44b5f076af32ab3793
Cédric Krier <ced@b2ck.com>
2018-10-26 23:24:09 +0200

    Fix argument order to Bus.publish in notify
    issue7807
    review51571002

--- trytond/tests/test_bus.py
+++ trytond/tests/test_bus.py
@@ -1,9 +1,14 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
+import time
 import unittest
 from unittest.mock import patch
 
-from trytond.bus import _MessageQueue
+from trytond import bus, backend
+from trytond.bus import _MessageQueue, notify, Bus
+from trytond.tests.test_tryton import (
+    activate_module, with_transaction, DB_NAME)
+from trytond.transaction import Transaction
 
 
 class MessageQueueTestCase(unittest.TestCase):
@@ -68,8 +73,75 @@ class MessageQueueTestCase(unittest.TestCase):
         self.assertEqual(content, {'message_id': 10})
 
 
+class BusTestCase(unittest.TestCase):
+    "Test Bus"
+
+    @classmethod
+    def setUpClass(cls):
+        activate_module('ir')
+        super().setUpClass()
+
+    def setUp(self):
+        super().setUp()
+
+        reset_polling_timeout = bus._long_polling_timeout
+        bus._long_polling_timeout = 1
+        self.addCleanup(
+            setattr, bus, '_long_polling_timeout', reset_polling_timeout)
+
+        reset_select_timeout = bus._select_timeout
+        bus._select_timeout = 1
+        self.addCleanup(
+            setattr, bus, '_select_timeout', reset_select_timeout)
+
+    def tearDown(self):
+        if DB_NAME in Bus._queues:
+            Bus._queues[DB_NAME]['timeout'] = 0
+            # Wait to let the listen thread stops
+            time.sleep(bus._select_timeout)
+        Bus._messages.clear()
+
+    @with_transaction()
+    def test_notify(self):
+        "Test notify"
+        notify("Test", "Message", user=1)
+
+    @unittest.skipIf(backend.name() == 'sqlite', 'SQLite has not channel')
+    def test_subscribe_nothing(self):
+        "Test subscribe with nothing"
+        response = Bus.subscribe(DB_NAME, ['user:1'])
+
+        self.assertEqual(response, {'message': None, 'channel': None})
+
+    @unittest.skipIf(backend.name() == 'sqlite', 'SQLite has not channel')
+    def test_subscribe_message(self):
+        "Test subscribe with message"
+        Bus.subscribe(DB_NAME, ['user:1'])
+
+        transaction = Transaction()
+        with transaction.start(DB_NAME, 1):
+            notify("Test", "Message", user=1)
+            transaction.commit()
+        # Let the listen thread registers the message
+        time.sleep(1)
+        response = Bus.subscribe(DB_NAME, ['user:1'])
+
+        self.assertTrue(response['message'].pop('message_id'))
+        self.assertEqual(response, {
+                'message': {
+                    'type': 'notification',
+                    'title': "Test",
+                    'body': "Message",
+                    'priority': 1,
+                    },
+                'channel': 'user:1',
+                })
+
+
 def suite():
     suite_ = unittest.TestSuite()
     suite_.addTests(unittest.TestLoader().loadTestsFromTestCase(
             MessageQueueTestCase))
+    suite_.addTests(unittest.TestLoader().loadTestsFromTestCase(
+            BusTestCase))
     return suite_
