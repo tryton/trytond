266eaa21bbb0fefdf201e16862741056ce306e04
Cédric Krier <ced@b2ck.com>
2018-10-31 18:13:02 +0100

    Re-browse the filtered menus
    It is needed to avoid access right errors when filtered records are access
    because they share the same local cache.
    
    issue7806
    review45771002
    (grafted from 2fd3fc52a2b354d38fa84188486e370f1dde0052)
    
    --HG--
    branch : 4.0

--- trytond/ir/ui/menu.py
+++ trytond/ir/ui/menu.py
@@ -190,8 +190,9 @@ class UIMenu(ModelSQL, ModelView):
                 parents.update(cls.search([
                             ('id', 'in', list(sub_parent_ids)),
                             ]))
-            menus = [x for x in menus
-                if (x.parent and x.parent in parents) or not x.parent]
+            # Re-browse to avoid side-cache access
+            menus = cls.browse([x.id for x in menus
+                    if (x.parent and x.parent in parents) or not x.parent])
 
         if count:
             return len(menus)
