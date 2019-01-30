22c2506b5d6e9c342bd4f15e70ef52860a2664b2
Cédric Krier <ced@b2ck.com>
2019-01-30 10:04:11 +0100

    Do not keeping lock when relaunch listener and join listener in test
    We can not recursively call Bus._listen when holding the lock because it
    blocks all subscription until the listener stops.
    The tearDown of the test should ensure the listener is really stopped before
    ending.
    
    issue8039
    review52611002

--- trytond/bus.py
+++ trytond/bus.py
@@ -95,6 +95,7 @@ class LongPollingBus:
             if start_listener:
                 listener = threading.Thread(
                     target=cls._listen, args=(database,), daemon=True)
+                cls._queues[database]['listener'] = listener
                 listener.start()
 
         messages = cls._messages.get(database)
@@ -191,7 +192,10 @@ class LongPollingBus:
                 del cls._queues[database]
             else:
                 # A query arrived between the end of the while and here
-                cls._listen(database)
+                listener = threading.Thread(
+                    target=cls._listen, args=(database,), daemon=True)
+                cls._queues[database]['listener'] = listener
+                listener.start()
 
     @classmethod
     def publish(cls, channel, message):
