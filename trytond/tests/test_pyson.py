79273cebcb6ef4591af2833f93d254ef14ffd28b
Cédric Krier <ced@b2ck.com>
2019-06-12 19:15:00 +0200

    Skip tests relying on assertion when python is optimized
    issue8381
    review271471002
    (grafted from 136794626cf54bb41a048d555e58df7cdd4efde1)
    
    --HG--
    branch : 4.4

--- trytond/tests/test_pyson.py
+++ trytond/tests/test_pyson.py
@@ -4,6 +4,8 @@
 
 import unittest
 import datetime
+import sys
+
 from trytond import pyson
 
 
@@ -106,11 +108,12 @@ class PYSONTestCase(unittest.TestCase):
             's': [True, False],
             })
 
-        self.assertRaises(AssertionError, pyson.And, 'test', False)
-        self.assertRaises(AssertionError, pyson.And, True, 'test')
-        self.assertRaises(AssertionError, pyson.And, True, False, 'test')
-        self.assertRaises(AssertionError, pyson.And, True)
-        self.assertRaises(AssertionError, pyson.And)
+        if not sys.flags.optimize:
+            self.assertRaises(AssertionError, pyson.And, 'test', False)
+            self.assertRaises(AssertionError, pyson.And, True, 'test')
+            self.assertRaises(AssertionError, pyson.And, True, False, 'test')
+            self.assertRaises(AssertionError, pyson.And, True)
+            self.assertRaises(AssertionError, pyson.And)
 
         self.assertEqual(pyson.And(True, False).types(), set([bool]))
 
@@ -151,11 +154,12 @@ class PYSONTestCase(unittest.TestCase):
             's': [True, False],
             })
 
-        self.assertRaises(AssertionError, pyson.Or, 'test', False)
-        self.assertRaises(AssertionError, pyson.Or, True, 'test')
-        self.assertRaises(AssertionError, pyson.Or, True, False, 'test')
-        self.assertRaises(AssertionError, pyson.Or, True)
-        self.assertRaises(AssertionError, pyson.Or)
+        if not sys.flags.optimize:
+            self.assertRaises(AssertionError, pyson.Or, 'test', False)
+            self.assertRaises(AssertionError, pyson.Or, True, 'test')
+            self.assertRaises(AssertionError, pyson.Or, True, False, 'test')
+            self.assertRaises(AssertionError, pyson.Or, True)
+            self.assertRaises(AssertionError, pyson.Or)
 
         self.assertEqual(pyson.Or(True, False).types(), set([bool]))
 
@@ -197,7 +201,8 @@ class PYSONTestCase(unittest.TestCase):
             's2': 'test',
             })
 
-        self.assertRaises(AssertionError, pyson.Equal, 'test', True)
+        if not sys.flags.optimize:
+            self.assertRaises(AssertionError, pyson.Equal, 'test', True)
 
         self.assertEqual(pyson.Equal('test', 'test').types(), set([bool]))
 
@@ -219,9 +224,10 @@ class PYSONTestCase(unittest.TestCase):
             'e': False,
             })
 
-        self.assertRaises(AssertionError, pyson.Greater, 'test', 0)
-        self.assertRaises(AssertionError, pyson.Greater, 1, 'test')
-        self.assertRaises(AssertionError, pyson.Greater, 1, 0, 'test')
+        if not sys.flags.optimize:
+            self.assertRaises(AssertionError, pyson.Greater, 'test', 0)
+            self.assertRaises(AssertionError, pyson.Greater, 1, 'test')
+            self.assertRaises(AssertionError, pyson.Greater, 1, 0, 'test')
 
         self.assertEqual(pyson.Greater(1, 0).types(), set([bool]))
 
@@ -260,9 +266,10 @@ class PYSONTestCase(unittest.TestCase):
             'e': False,
             })
 
-        self.assertRaises(AssertionError, pyson.Less, 'test', 1)
-        self.assertRaises(AssertionError, pyson.Less, 0, 'test')
-        self.assertRaises(AssertionError, pyson.Less, 0, 1, 'test')
+        if not sys.flags.optimize:
+            self.assertRaises(AssertionError, pyson.Less, 'test', 1)
+            self.assertRaises(AssertionError, pyson.Less, 0, 'test')
+            self.assertRaises(AssertionError, pyson.Less, 0, 1, 'test')
 
         self.assertEqual(pyson.Less(0, 1).types(), set([bool]))
 
@@ -301,8 +308,9 @@ class PYSONTestCase(unittest.TestCase):
             'e': 'bar',
             })
 
-        self.assertRaises(AssertionError, pyson.If, 'test', 'foo', 'bar')
-        self.assertRaises(AssertionError, pyson.If, True, 'foo', False)
+        if not sys.flags.optimize:
+            self.assertRaises(AssertionError, pyson.If, 'test', 'foo', 'bar')
+            self.assertRaises(AssertionError, pyson.If, True, 'foo', False)
 
         self.assertEqual(pyson.If(True, 'foo', 'bar').types(),
             set([basestring]))
@@ -326,8 +334,10 @@ class PYSONTestCase(unittest.TestCase):
             'd': 'default',
             })
 
-        self.assertRaises(AssertionError, pyson.Get, 'test', 'foo', 'default')
-        self.assertRaises(AssertionError, pyson.Get, {}, 1, 'default')
+        if not sys.flags.optimize:
+            self.assertRaises(
+                AssertionError, pyson.Get, 'test', 'foo', 'default')
+            self.assertRaises(AssertionError, pyson.Get, {}, 1, 'default')
 
         self.assertEqual(pyson.Get({}, 'foo', 'default').types(),
             set([basestring]))
@@ -356,8 +366,9 @@ class PYSONTestCase(unittest.TestCase):
             'v': {'foo': 'bar'},
             })
 
-        self.assertRaises(AssertionError, pyson.In, object(), {})
-        self.assertRaises(AssertionError, pyson.In, 'test', 'foo')
+        if not sys.flags.optimize:
+            self.assertRaises(AssertionError, pyson.In, object(), {})
+            self.assertRaises(AssertionError, pyson.In, 'test', 'foo')
 
         self.assertEqual(pyson.In('foo', {}).types(), set([bool]))
 
@@ -406,18 +417,19 @@ class PYSONTestCase(unittest.TestCase):
             'dd': -7
             })
 
-        self.assertRaises(AssertionError, pyson.Date, 'test', 1, 12, -1, 12,
-                -7)
-        self.assertRaises(AssertionError, pyson.Date, 2010, 'test', 12, -1, 12,
-                -7)
-        self.assertRaises(AssertionError, pyson.Date, 2010, 1, 'test', -1, 12,
-                -7)
-        self.assertRaises(AssertionError, pyson.Date, 2010, 1, 12, 'test', 12,
-                -7)
-        self.assertRaises(AssertionError, pyson.Date, 2010, 1, 12, -1, 'test',
-                -7)
-        self.assertRaises(AssertionError, pyson.Date, 2010, 1, 12, -1, 12,
-                'test')
+        if not sys.flags.optimize:
+            self.assertRaises(
+                AssertionError, pyson.Date, 'test', 1, 12, -1, 12, -7)
+            self.assertRaises(
+                AssertionError, pyson.Date, 2010, 'test', 12, -1, 12, -7)
+            self.assertRaises(
+                AssertionError, pyson.Date, 2010, 1, 'test', -1, 12, -7)
+            self.assertRaises(
+                AssertionError, pyson.Date, 2010, 1, 12, 'test', 12, -7)
+            self.assertRaises(
+                AssertionError, pyson.Date, 2010, 1, 12, -1, 'test', -7)
+            self.assertRaises(
+                AssertionError, pyson.Date, 2010, 1, 12, -1, 12, 'test')
 
         self.assertEqual(pyson.Date(2010, 1, 12, -1, 12, -7).types(),
             set([datetime.date]))
@@ -470,34 +482,49 @@ class PYSONTestCase(unittest.TestCase):
                 'dms': 1,
                 })
 
-        self.assertRaises(AssertionError, pyson.DateTime, 'test', 1, 12, 10,
-                30, 20, 0, -1, 12, -7, 2, 15, 30, 1)
-        self.assertRaises(AssertionError, pyson.DateTime, 2010, 'test', 12, 10,
-                30, 20, 0, -1, 12, -7, 2, 15, 30, 1)
-        self.assertRaises(AssertionError, pyson.DateTime, 2010, 1, 'test', 10,
-                30, 20, 0, -1, 12, -7, 2, 15, 30, 1)
-        self.assertRaises(AssertionError, pyson.DateTime, 2010, 1, 12, 'test',
-                30, 20, 0, -1, 12, -7, 2, 15, 30, 1)
-        self.assertRaises(AssertionError, pyson.DateTime, 2010, 1, 12, 10,
-                'test', 20, 0, -1, 12, -7, 2, 15, 30, 1)
-        self.assertRaises(AssertionError, pyson.DateTime, 2010, 1, 12, 10, 30,
-                'test', 0, -1, 12, -7, 2, 15, 30, 1)
-        self.assertRaises(AssertionError, pyson.DateTime, 2010, 1, 12, 10, 30,
-                20, 'test', -1, 12, -7, 2, 15, 30, 1)
-        self.assertRaises(AssertionError, pyson.DateTime, 2010, 1, 12, 10, 30,
-                20, 0, 'test', 12, -7, 2, 15, 30, 1)
-        self.assertRaises(AssertionError, pyson.DateTime, 2010, 1, 12, 10, 30,
-                20, 0, -1, 'test', -7, 2, 15, 30, 1)
-        self.assertRaises(AssertionError, pyson.DateTime, 2010, 1, 12, 10, 30,
-                20, 0, -1, 12, 'test', 2, 15, 30, 1)
-        self.assertRaises(AssertionError, pyson.DateTime, 2010, 1, 12, 10, 30,
-                20, 0, -1, 12, -7, 'test', 15, 30, 1)
-        self.assertRaises(AssertionError, pyson.DateTime, 2010, 1, 12, 10, 30,
-                20, 0, -1, 12, -7, 2, 'test', 30, 1)
-        self.assertRaises(AssertionError, pyson.DateTime, 2010, 1, 12, 10, 30,
-                20, 0, -1, 12, -7, 2, 15, 'test', 1)
-        self.assertRaises(AssertionError, pyson.DateTime, 2010, 1, 12, 10, 30,
-                20, 0, -1, 12, -7, 2, 15, 30, 'test')
+        if not sys.flags.optimize:
+            self.assertRaises(
+                AssertionError, pyson.DateTime,
+                'test', 1, 12, 10, 30, 20, 0, -1, 12, -7, 2, 15, 30, 1)
+            self.assertRaises(
+                AssertionError, pyson.DateTime,
+                2010, 'test', 12, 10, 30, 20, 0, -1, 12, -7, 2, 15, 30, 1)
+            self.assertRaises(
+                AssertionError, pyson.DateTime,
+                2010, 1, 'test', 10, 30, 20, 0, -1, 12, -7, 2, 15, 30, 1)
+            self.assertRaises(
+                AssertionError, pyson.DateTime,
+                2010, 1, 12, 'test', 30, 20, 0, -1, 12, -7, 2, 15, 30, 1)
+            self.assertRaises(
+                AssertionError, pyson.DateTime,
+                2010, 1, 12, 10, 'test', 20, 0, -1, 12, -7, 2, 15, 30, 1)
+            self.assertRaises(
+                AssertionError, pyson.DateTime,
+                2010, 1, 12, 10, 30, 'test', 0, -1, 12, -7, 2, 15, 30, 1)
+            self.assertRaises(
+                AssertionError, pyson.DateTime,
+                2010, 1, 12, 10, 30, 20, 'test', -1, 12, -7, 2, 15, 30, 1)
+            self.assertRaises(
+                AssertionError, pyson.DateTime,
+                2010, 1, 12, 10, 30, 20, 0, 'test', 12, -7, 2, 15, 30, 1)
+            self.assertRaises(
+                AssertionError, pyson.DateTime,
+                2010, 1, 12, 10, 30, 20, 0, -1, 'test', -7, 2, 15, 30, 1)
+            self.assertRaises(
+                AssertionError, pyson.DateTime,
+                2010, 1, 12, 10, 30, 20, 0, -1, 12, 'test', 2, 15, 30, 1)
+            self.assertRaises(
+                AssertionError, pyson.DateTime,
+                2010, 1, 12, 10, 30, 20, 0, -1, 12, -7, 'test', 15, 30, 1)
+            self.assertRaises(
+                AssertionError, pyson.DateTime,
+                2010, 1, 12, 10, 30, 20, 0, -1, 12, -7, 2, 'test', 30, 1)
+            self.assertRaises(
+                AssertionError, pyson.DateTime,
+                2010, 1, 12, 10, 30, 20, 0, -1, 12, -7, 2, 15, 'test', 1)
+            self.assertRaises(
+                AssertionError, pyson.DateTime,
+                2010, 1, 12, 10, 30, 20, 0, -1, 12, -7, 2, 15, 30, 'test')
 
         self.assertEqual(pyson.DateTime(2010, 1, 12, 10, 30, 20, 0,
             -1, 12, -7, 2, 15, 30, 1).types(), set([datetime.datetime]))
@@ -558,7 +585,8 @@ class PYSONTestCase(unittest.TestCase):
                 'v': [1, 2, 3],
                 })
 
-        self.assertRaises(AssertionError, pyson.Len, object())
+        if not sys.flags.optimize:
+            self.assertRaises(AssertionError, pyson.Len, object())
 
         self.assertEqual(pyson.Len([1, 2, 3]).types(), set([int]))
 
