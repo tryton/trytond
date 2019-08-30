0bad9beed26133ffec063ca9911b19fb8bbad34a
Cédric Krier <ced@b2ck.com>
2019-08-30 11:32:32 +0200

    Use MagicMock for message when testing sendmail
    Since issue8596, sendmail test message contains Date header so message must
    implement the magic method.
    (grafted from 464864aedc0460dd6717d6197eb736bfa2433169)
    
    --HG--
    branch : 4.6

--- trytond/tests/test_sendmail.py
+++ trytond/tests/test_sendmail.py
@@ -4,7 +4,7 @@ import unittest
 import smtplib
 import sys
 from email.message import Message
-from mock import Mock, patch, call
+from mock import Mock, MagicMock, patch, call
 
 from trytond.sendmail import (
     sendmail_transactional, sendmail, SMTPDataManager, get_smtp_server)
@@ -22,7 +22,7 @@ class SendmailTestCase(unittest.TestCase):
     @with_transaction()
     def test_sendmail_transactional(self):
         'Test sendmail_transactional'
-        message = Mock()
+        message = MagicMock()
         datamanager = Mock()
         sendmail_transactional(
             'tryton@example.com', 'foo@example.com', message,
@@ -33,7 +33,7 @@ class SendmailTestCase(unittest.TestCase):
 
     def test_sendmail(self):
         'Test sendmail'
-        message = Mock()
+        message = MagicMock()
         server = Mock()
         sendmail(
             'tryton@example.com', 'foo@example.com', message, server=server)
@@ -91,8 +91,8 @@ class SendmailTestCase(unittest.TestCase):
         # multiple join must return the same
         self.assertEqual(transaction.join(SMTPDataManager()), datamanager)
 
-        msg1 = Mock(Message)
-        msg2 = Mock(Message)
+        msg1 = MagicMock(Message)
+        msg2 = MagicMock(Message)
         datamanager.put('foo@example.com', 'bar@example.com', msg1)
         datamanager.put('bar@example.com', 'foo@example.com', msg2)
 
@@ -107,7 +107,8 @@ class SendmailTestCase(unittest.TestCase):
 
         server.reset_mock()
 
-        datamanager.put('foo@example.com', 'bar@example.com', Mock(Message))
+        datamanager.put(
+            'foo@example.com', 'bar@example.com', MagicMock(Message))
         transaction.rollback()
 
         server.sendmail.assert_not_called()
