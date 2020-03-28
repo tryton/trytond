11d66591da9a470ebbbda5493b2a94a5ce595b1f
Cédric Krier <ced@b2ck.com>
2020-03-28 23:37:53 +0100

    Remove unused time import
    issue8342

--- trytond/tests/test_trigger.py
+++ trytond/tests/test_trigger.py
@@ -2,7 +2,6 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 import unittest
-import time
 import datetime
 from itertools import combinations
 
