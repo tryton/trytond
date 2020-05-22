3aaf6c0794cba8d0a9a2009ca837d0719d17048d
Sergi Almacellas Abellana <sergi@koolpi.com>
2020-05-22 11:27:27 +0200

    Ensure User application key is different on bunch creation
    issue9306
    review321301002
    (grafted from 8021e386894bd1e907d5a855c1ba2cf3af3a3bae)
    
    --HG--
    branch : 5.4

--- trytond/res/user.py
+++ trytond/res/user.py
@@ -901,6 +901,8 @@ class UserApplication(Workflow, ModelSQL, ModelView):
                     'depends': ['state'],
                     },
                 })
+        # Do not cache default_key as it depends on time
+        cls.__rpc__['default_get'].cache = None
 
     @classmethod
     def default_key(cls):
@@ -945,6 +947,11 @@ class UserApplication(Workflow, ModelSQL, ModelView):
     def create(cls, vlist):
         pool = Pool()
         User = pool.get('res.user')
+        vlist = [v.copy() for v in vlist]
+        for values in vlist:
+            # Ensure we get a different key for each record
+            # default methods are called only once
+            values.setdefault('key', cls.default_key())
         applications = super(UserApplication, cls).create(vlist)
         User._get_preferences_cache.clear()
         return applications
