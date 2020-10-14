4dc2e00ea47a577b0646f6265b10ee11c0060ea3
Cédric Krier <ced@b2ck.com>
2020-10-14 14:43:40 +0200

    Restore English language on tear down
    The translations tests change the default language on setup and commit the
    change. So we must restore the initial state of the database at the end of the
    test case.
    
    issue9702
    review312521002

--- trytond/ir/configuration.py
+++ trytond/ir/configuration.py
@@ -31,3 +31,19 @@ class Configuration(ModelSingleton, ModelSQL):
     def check(self):
         "Check configuration coherence on pool initialisation"
         pass
+
+    @classmethod
+    def create(cls, vlist):
+        records = super().create(vlist)
+        cls._get_language_cache.clear()
+        return records
+
+    @classmethod
+    def write(cls, *args):
+        super().write(*args)
+        cls._get_language_cache.clear()
+
+    @classmethod
+    def delete(cls, records):
+        super().delete(records)
+        cls._get_language_cache.clear()
