d52625c0bd35dc00e3bdceef52025816e2d83b3d
Cédric Krier <ced@b2ck.com>
2021-05-05 18:09:08 +0200

    Skip avatar test if PIL is not installed (grafted from 4d216b768a9c802ddd8c285c278dc9c2df50112c)
    --HG--
    branch : 6.0

--- trytond/tests/test_res.py
+++ trytond/tests/test_res.py
@@ -2,6 +2,11 @@
 # this repository contains the full copyright notices and license terms.
 import unittest
 
+try:
+    import PIL
+except ImportError:
+    PIL = None
+
 from trytond.pool import Pool
 
 from .test_tryton import ModuleTestCase, with_transaction
@@ -11,6 +16,7 @@ class ResTestCase(ModuleTestCase):
     'Test res module'
     module = 'res'
 
+    @unittest.skipUnless(PIL, "Avatars are not generated without PIL")
     @with_transaction()
     def test_user_avatar(self):
         pool = Pool()
