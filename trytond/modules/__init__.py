87f4760940506c034687a3df3336e664444c1f3d
Cédric Krier <ced@b2ck.com>
2021-08-19 22:29:09 +0200

    Clear the cache only if modules are updated
    issue10646
    review365731002

--- trytond/modules/__init__.py
+++ trytond/modules/__init__.py
@@ -300,9 +300,10 @@ def load_module_graph(graph, pool, update=None, lang=None):
             (module, to_delete) = modules_todo.pop()
             convert.post_import(pool, module, to_delete)
 
-        # Ensure cache is clear for other instances
-        Cache.clear_all()
-        Cache.refresh_pool(transaction)
+        if update:
+            # Ensure cache is clear for other instances
+            Cache.clear_all()
+            Cache.refresh_pool(transaction)
     logger.info('all modules loaded')
 
 
