c53769b1e0f148489f6727e3f9bb98d966695daa
Cédric Krier <ced@b2ck.com>
2022-02-14 23:12:00 +0100

    Avoid concurrency failure in cache with channel test
    We need to ensure that the listener is running on the return of the transaction
    start otherwise notification can be missed.
    We must also just from time to time check that the listener is still the
    current listener.
    And finally we can not wait an arbitrary time for cache sync, instead we must
    rely on the _clean_last being reset with the sync happens.
    
    issue9413
    review364831002

--- trytond/cache.py
+++ trytond/cache.py
@@ -6,6 +6,7 @@ import logging
 import os
 import select
 import threading
+import time
 from collections import OrderedDict, defaultdict
 from datetime import datetime
 from weakref import WeakKeyDictionary
@@ -220,6 +221,9 @@ class MemoryCache(BaseCache):
                     cls._listener[pid, dbname] = listener = threading.Thread(
                         target=cls._listen, args=(dbname,), daemon=True)
                     listener.start()
+                    while (not getattr(listener, 'listening', False)
+                            and listener.is_alive()):
+                        time.sleep(.01)
             return
         if (datetime.now() - cls._clean_last).total_seconds() < _clear_timeout:
             return
@@ -355,9 +359,10 @@ class MemoryCache(BaseCache):
         try:
             cursor = conn.cursor()
             cursor.execute('LISTEN "%s"' % cls._channel)
+            current_thread.listening = True
 
             while cls._listener.get((pid, dbname)) == current_thread:
-                readable, _, _ = select.select([conn], [], [])
+                readable, _, _ = select.select([conn], [], [], 60)
                 if not readable:
                     continue
 
