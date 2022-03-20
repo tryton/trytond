0446c5d5cf540a1243ce7bc8bc78fd4119cdfae4
Cédric Krier <ced@b2ck.com>
2022-03-20 01:32:04 +0100

    Backed out changeset d645aee76ab0
    issue11262

--- trytond/model/multivalue.py
+++ trytond/model/multivalue.py
@@ -45,10 +45,8 @@ class MultiValueMixin(object):
         return record
 
     def __values(self, field, pattern, match_none=True):
-        Value = self.multivalue_model(field)
-        # Limit the cache to matching records
-        return Value.browse((v for v in self.multivalue_records(field)
-            if v.match(pattern, match_none=match_none)))
+        return [v for v in self.multivalue_records(field)
+            if v.match(pattern, match_none=match_none)]
 
     def get_multivalue(self, name, **pattern):
         Value = self.multivalue_model(name)
