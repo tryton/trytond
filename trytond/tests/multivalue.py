5e0ccfd02322d19ccce6eeaa6b5df181332deaf8
José Antonio Díaz <tiyujopite1@gmail.com>
2022-10-15 22:08:25 +0200

    Support MultiSelection in MultiValue field
    issue11730
    review427811003
    (grafted from bd9ad6eedf5c1f4dc135563fce5fff34999ee2ac)
    
    --HG--
    branch : 6.4

--- trytond/tests/multivalue.py
+++ trytond/tests/multivalue.py
@@ -9,13 +9,34 @@ class ModelMultiValue(ModelSQL, MultiValueMixin):
     __name__ = 'test.model_multivalue'
     value = fields.MultiValue(fields.Char("Value"))
     value_default = fields.MultiValue(fields.Char("Value Default"))
+    value_many2one = fields.MultiValue(
+        fields.Many2One('test.model_multivalue.target', "Value Many2One"))
+    value_multiselection = fields.MultiValue(
+        fields.MultiSelection([
+                ('foo', "Foo"),
+                ('bar', "Bar"),
+                ], "Value Multi Selection"))
+    value_reference = fields.MultiValue(
+        fields.Reference(
+            "Value Reference",
+            selection=[
+                (None, ""),
+                ('test.model_multivalue.target', "Target"),
+                ]))
     values = fields.One2Many(
         'test.model_multivalue.value', 'record', "Values")
 
     @classmethod
     def multivalue_model(cls, field):
         pool = Pool()
-        if field == 'value_default':
+        multivalue_fields = {
+            'value',
+            'value_default',
+            'value_many2one',
+            'value_multiselection',
+            'value_reference'
+            }
+        if field in multivalue_fields:
             return pool.get('test.model_multivalue.value')
         return super().multivalue_model(field)
 
@@ -27,6 +48,10 @@ class ModelMultiValue(ModelSQL, MultiValueMixin):
     def default_value_default(cls, **pattern):
         return "other default"
 
+    @classmethod
+    def default_value_multiselection(cls, **pattern):
+        return ('foo',)
+
 
 class ModelValue(ModelSQL, ValueMixin):
     "Model Value"
@@ -36,10 +61,29 @@ class ModelValue(ModelSQL, ValueMixin):
     condition = fields.Char("Condition")
     value = fields.Char("Value")
     value_default = fields.Char("Value Default")
+    value_many2one = fields.Many2One(
+        'test.model_multivalue.target', "Value Many2One")
+    value_multiselection = fields.MultiSelection([
+            ('foo', "Foo"),
+            ('bar', "Bar"),
+            ], "Value Multi Selection")
+    value_reference = fields.Reference(
+        "Value Reference",
+        selection=[
+            (None, ""),
+            ('test.model_multivalue.target', "Target"),
+            ])
+
+
+class ModelMultiValueTarget(ModelSQL):
+    "Model MultiValue Target"
+    __name__ = 'test.model_multivalue.target'
+    name = fields.Char("Name")
 
 
 def register(module):
     Pool.register(
         ModelMultiValue,
         ModelValue,
+        ModelMultiValueTarget,
         module=module, type_='model')
